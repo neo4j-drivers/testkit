62f5b00282511c5f60f7b9c9232b075dea82312c
Peter Wilhelmsson <peter.wilhelmsson@neo4j.com>
2020-08-13 09:57:04 +0200

    Compile Go on 32 bits architecture

--- driver/go/build.py
+++ driver/go/build.py
@@ -5,9 +5,9 @@ Responsible for building driver and test backend.
 import os, subprocess, shutil
 
 
-def run(args):
+def run(args, env=None):
     subprocess.run(
-        args, universal_newlines=True, stderr=subprocess.STDOUT, check=True)
+        args, universal_newlines=True, stderr=subprocess.STDOUT, check=True, env=env)
 
 if __name__ == "__main__":
     # Setup a Go path environment to build in (since we allow Go 1.10)
@@ -19,3 +19,11 @@ if __name__ == "__main__":
     os.environ["GOPATH"] = goPath
     run(["go", "get", "-v", "github.com/neo4j/neo4j-go-driver/..."])
     run(["go", "install","-v", "github.com/neo4j/neo4j-go-driver/testkit-backend"])
+
+    # Compile for 32 bits ARM to make sure it builds
+    print("Building for 32 bits")
+    arm32Env = os.environ.copy()
+    arm32Env["GOOS"] = "linux"
+    arm32Env["GOARCH"] = "arm"
+    arm32Env["GOARM"] = "7"
+    run(["go", "build", "github.com/neo4j/neo4j-go-driver/neo4j"], env=arm32Env)
