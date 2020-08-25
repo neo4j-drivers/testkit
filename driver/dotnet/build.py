728649dc127069c732a399383c89abfa811dd983
AndyHeap-NeoTech <andrew.heap@neotechnology.com>
2020-08-25 11:52:42 +0100

    Implemented code to run .net pure unit tests.

--- driver/dotnet/build.py
+++ driver/dotnet/build.py
@@ -9,7 +9,6 @@ def run(args):
     subprocess.run(
         args, universal_newlines=True, stderr=subprocess.STDOUT, check=True)
 
-
 if __name__ == "__main__":
     run(["dotnet", "restore", "--disable-parallel", "-v", "n", "Neo4j.Driver/Neo4j.Driver.sln"])
     run(["dotnet", "msbuild", "Neo4j.Driver/Neo4j.Driver.sln"])
--- driver/dotnet/unittests.py
+++ driver/dotnet/unittests.py
@@ -1,3 +1,21 @@
 
+"""
+Executed in Go driver container.
+Responsible for running unit tests.
+Assumes driver has been setup by build script prior to this.
+"""
+import os, subprocess
+
+root_package = "github.com/neo4j/neo4j-dotnet-driver/neo4j"
+
+def run(args):
+    subprocess.run(
+        args, universal_newlines=True, stderr=subprocess.STDOUT, check=True)
+
 if __name__ == "__main__":
-    pass
+
+    # run the dotnet test framework from the neo4j.driver/neo4j.driver.tests directory.
+    wd = os.getcwd()
+    os.chdir("Neo4j.Driver/Neo4j.Driver.Tests")
+    run(["dotnet", "test", "Neo4j.Driver.Tests.csproj"])
+    os.chdir(wd)
