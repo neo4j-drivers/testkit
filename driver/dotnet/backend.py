fc1323c785db23861f45db740c5febe52ba8d579
Peter Wilhelmsson <peter.wilhelmsson@neo4j.com>
2020-08-26 16:16:59 +0200

    Revert binary name to be netcoreap2.0

--- driver/dotnet/backend.py
+++ driver/dotnet/backend.py
@@ -6,6 +6,6 @@ Responsible for starting the test backend.
 import os, subprocess
 
 if __name__ == "__main__":
-    backend_path = os.path.join(".", "Neo4j.Driver", "Neo4j.Driver.Tests.TestBackend", "bin", "Debug", "netcoreapp3.1", "Neo4j.Driver.Tests.TestBackend")
+    backend_path = os.path.join(".", "Neo4j.Driver", "Neo4j.Driver.Tests.TestBackend", "bin", "Debug", "netcoreapp2.0", "Neo4j.Driver.Tests.TestBackend")
     logfile_path = os.path.join("..", "artifacts", "backend.log")
     subprocess.check_call([backend_path, "0.0.0.0", "9876", logfile_path])
