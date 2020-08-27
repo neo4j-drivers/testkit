75231dfdf84173685e6cb4386d19592a70eb0f23
AndyHeap-NeoTech <andrew.heap@neotechnology.com>
2020-08-27 16:29:22 +0100

    Command line changes to enable the correct publishing and then running of the TestBackend for .Net

--- driver/dotnet/backend.py
+++ driver/dotnet/backend.py
@@ -6,6 +6,6 @@ Responsible for starting the test backend.
 import os, subprocess
 
 if __name__ == "__main__":
-    backend_path = os.path.join(".", "Neo4j.Driver", "Neo4j.Driver.Tests.TestBackend", "bin", "Debug", "netcoreapp3.1", "Neo4j.Driver.Tests.TestBackend")
+    backend_path = os.path.join("Neo4j.Driver", "Neo4j.Driver.Tests.TestBackend", "bin", "Publish", "Neo4j.Driver.Tests.TestBackend.dll")
     logfile_path = os.path.join("..", "artifacts", "backend.log")
-    subprocess.check_call([backend_path, "0.0.0.0", "9876", logfile_path])
+    subprocess.check_call(["dotnet", backend_path, "0.0.0.0", "9876", logfile_path])
