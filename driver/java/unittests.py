9e41c24f7d78c691af3ad32072eb908c30958045
Peter Wilhelmsson <peter.wilhelmsson@neo4j.com>
2020-09-08 12:45:19 +0200

    Run 'pure' Java unit tests

--- driver/java/unittests.py
+++ driver/java/unittests.py
@@ -4,5 +4,11 @@ Responsible for running unit tests.
 Assumes driver has been setup by build script prior to this.
 """
 
+import os, subprocess
+
+def run(args):
+    subprocess.run(
+        args, universal_newlines=True, stderr=subprocess.STDOUT, check=True)
+
 if __name__ == "__main__":
-    pass
+    run(["mvn", "test"])
