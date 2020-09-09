c51e289580b702552d84f6c7dc98e0b8d68ba1c9
Peter Wilhelmsson <peter.wilhelmsson@neo4j.com>
2020-09-09 16:13:47 +0200

    Initial Javascript support

---
+++ driver/javascript/build.py
@@ -0,0 +1,14 @@
+"""
+Executed in Javascript driver container.
+Responsible for building driver and test backend.
+"""
+import os, subprocess, shutil
+
+
+def run(args, env=None):
+    subprocess.run(
+        args, universal_newlines=True, stderr=subprocess.STDOUT, check=True, env=env)
+
+if __name__ == "__main__":
+    run(["gulp", "nodejs"])
+
