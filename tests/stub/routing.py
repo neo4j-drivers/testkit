b877e58fcc7922b6cfef28c30de8fe867ee0091e
AndyHeap-NeoTech <andrew.heap@neotechnology.com>
2020-10-12 12:40:43 +0100

    Reverting changes so tests work and will refactor in a different branch going forward.

--- tests/stub/routing.py
+++ tests/stub/routing.py
@@ -3,7 +3,6 @@ import unittest
 from tests.shared import *
 from tests.stub.shared import *
 from nutkit.frontend import Driver, AuthorizationToken
-from collections import OrderedDict
 import nutkit.protocol as types
 
 script_router = """
@@ -77,95 +76,51 @@ S: SUCCESS {}
 """
 
 
-"""vars_v3 = {
+vars_v3 = {
     "#VERSION#":           '3',
-    "#GET_ROUTING_TABLE#": 'RUN \"CALL dbms.cluster.routing.getRoutingTable($context)\" {\"context\": {\"address\": \"#ADDRESS_STR\"}} {#MODE_STR}',
+    "#GET_ROUTING_TABLE#": 'RUN "CALL dbms.cluster.routing.getRoutingTable($context)" {"context": {}} {"mode": "r"}',
     "#PULL#":              'PULL_ALL',
     "#RUN_READ#":          'RUN "RETURN 1 as n" {} {"mode": "r"}',
     "#RUN_WRITE#":         'RUN "RETURN 1 as n" {} {}',
     "#BEGIN_READ#":        'BEGIN {"mode": "r"}',
     "#BEGIN_WRITE#":       'BEGIN {}',
+}
 
-    # Variables for language specifics
-    "#ADDRESS_STR": '',
-    "#MODE_STR": '"mode": "r", "db": "system"',
-}"""
-
-
-
-
-"""dbname = "adb"
+# Bolt version 4 and later needs named databases when querying for routing table.
+dbname = "adb"
 vars_v4 = {
     "#VERSION#":           '4.0',
-    "#GET_ROUTING_TABLE#": 'RUN \"CALL dbms.routing.getRoutingTable($context, $#DATABASE_STR)\" {\"context\": {\"address\": \"#ADDRESS_STR\"}, \"#DATABASE_STR\": \"%s\"} {#MODE_STR}' % (dbname),
+    "#GET_ROUTING_TABLE#": 'RUN "CALL dbms.routing.getRoutingTable($context, $db)" {"context": {}, "db": "%s"} {"mode": "r", "db": "system"}' % (dbname),
     "#PULL#":              'PULL {"n": 1000}',
     "#RUN_READ#":          'RUN "RETURN 1 as n" {} {"mode": "r", "db": "%s"}' % (dbname),
     "#RUN_WRITE#":         'RUN "RETURN 1 as n" {} {"db": "%s"}' % (dbname),
     "#BEGIN_READ#":        'BEGIN {"mode": "r", "db": "%s"}' % (dbname),
     "#BEGIN_WRITE#":       'BEGIN {"db": "%s"}' % (dbname),
-
-    # Variables for language specifics
-    "#DATABASE_STR": 'db',
-    "#ADDRESS_STR": '',
-    "#MODE_STR": '"mode": "r", "db": "system"',
-}"""
+}
 
 
 # Current version of the protocol. Older protocol version should be subclasses to make
 # it easier to remove when no longer supported.
 class Routing(unittest.TestCase):
-    def setUpVariables(self):
-        # Bolt version 4 and later needs named databases when querying for routing table.
-        self._dbname = "adb"
-
-        self._vars_v4 = OrderedDict()
-        self._vars_v4["#VERSION#"] = '4.0'
-        self._vars_v4["#GET_ROUTING_TABLE#"] = 'RUN \"CALL dbms.routing.getRoutingTable($context, $#DATABASE_STR)\" ' \
-                                               '{\"context\": {\"#ADDRESS_STR\"},' \
-                                               ' \"#DATABASE_STR\": \"%s\"} {#MODE_STR}' % (self._dbname)
-        self._vars_v4["#PULL#"] = 'PULL {"n": 1000}'
-        self._vars_v4["#RUN_READ#"] = 'RUN "RETURN 1 as n" {} {"mode"] = "r", "db"] = "%s"}' % (self._dbname)
-        self._vars_v4["#RUN_WRITE#"] = 'RUN "RETURN 1 as n" {} {"db"] = "%s"}' % (self._dbname)
-        self._vars_v4["#BEGIN_READ#"] = 'BEGIN {"mode"] = "r", "db"] = "%s"}' % (self._dbname)
-        self._vars_v4["#BEGIN_WRITE#"] = 'BEGIN {"db"] = "%s"}' % (self._dbname)
-
-        # Variables for language specifics
-        self._vars_v4["#DATABASE_STR"] = 'db'
-        self._vars_v4["#ADDRESS_STR"] = ''
-        self._vars_v4["#MODE_STR"] = '"mode": "r", "db": "system"'
-
     def setUp(self):
         self._backend = new_backend()
         self._driverName = get_driver_name()
-        self._routingServer = StubServer(9001)
-        self._readServer = StubServer(9002)
-        self._writeServer = StubServer(9003)
 
         # Bolt version hook
-        self.setUpVariables()
         self.setupVersion()
 
+        self._routingServer = StubServer(9001)
         self._vars["#HOST#"] = self._routingServer.host
-
-        self.runServer(self._routingServer)
-
+        self._routingServer.start(script=script_router, vars=self._vars)
+        self._readServer = StubServer(9002)
+        self._writeServer = StubServer(9003)
         uri = "neo4j://%s" % self._routingServer.address
         # Driver is configured to talk to "routing" stub server
         self._driver = Driver(self._backend, uri, AuthorizationToken(scheme="basic"))
 
     def setupVersion(self):
-        self._database = self._dbname
-        self._vars = self._vars_v4
-
-        # Now append language specific variables.
-        if self._driverName in ['dotnet']:
-            self._vars["#ADDRESS_STR"] = "address: " + os.environ.get("TEST_STUB_HOST") + ":#PORT"
-            self._vars["#DATABASE_STR"] = 'database'
-            self._vars["#PORT"] = ''
-
-    def runServer(self, server):
-        self._vars["#PORT"] = str(server.port)
-        server.start(script=script_router, vars=self._vars)
+        self._database = dbname
+        self._vars = vars_v4
 
     def tearDown(self):
         self._driver.close()
@@ -177,10 +132,9 @@ class Routing(unittest.TestCase):
     # Checks that routing is used to connect to correct server and that parameters for
     # session run is passed on to the target server (not the router).
     def test_session_run_read(self):
-        if self._driverName not in ['go', 'dotnet']:
+        if self._driverName not in ['go']:
             self.skipTest("Session with named database not implemented in backend")
-
-        self.runServer(self._readServer)
+        self._readServer.start(script=script_read, vars=self._vars)
         session = self._driver.session('r', database=self._database)
         session.run("RETURN 1 as n")
         session.close()
@@ -189,8 +143,7 @@ class Routing(unittest.TestCase):
     def test_tx_run_read(self):
         if self._driverName not in ['go']:
             self.skipTest("Session with named database not implemented in backend")
-
-        self.runServer(self._readServer)
+        self._readServer.start(script=script_tx_read, vars=self._vars)
         session = self._driver.session('r', database=self._database)
         tx = session.beginTransaction()
         tx.run("RETURN 1 as n")
@@ -201,8 +154,7 @@ class Routing(unittest.TestCase):
     def test_session_run_write(self):
         if self._driverName not in ['go']:
             self.skipTest("Session with named database not implemented in backend")
-
-        self.runServer(self._writeServer)
+        self._writeServer.start(script=script_write, vars=self._vars)
         session = self._driver.session('w', database=self._database)
         session.run("RETURN 1 as n")
         session.close()
@@ -211,8 +163,7 @@ class Routing(unittest.TestCase):
     def test_tx_run_write(self):
         if self._driverName not in ['go']:
             self.skipTest("Session with named database not implemented in backend")
-
-        self.runServer(self._writeServer)
+        self._writeServer.start(script=script_tx_write, vars=self._vars)
         session = self._driver.session('w', database=self._database)
         tx = session.beginTransaction()
         tx.run("RETURN 1 as n")
@@ -221,28 +172,7 @@ class Routing(unittest.TestCase):
 
 
 class RoutingV3(Routing):
-    def setUpVariables(self):
-        self._vars_v3 = OrderedDict()
-        self._vars_v3["#VERSION#"] = '3'
-        self._vars_v3["#GET_ROUTING_TABLE#"] = 'RUN \"CALL dbms.cluster.routing.getRoutingTable($context)\" {\"context\": {\"address\": \"#ADDRESS_STR\"}} {#MODE_STR}'
-        self._vars_v3["#PULL#"] = 'PULL_ALL'
-        self._vars_v3["#RUN_READ#"] = 'RUN "RETURN 1 as n" {} {"mode": "r"}'
-        self._vars_v3["#RUN_WRITE#"] = 'RUN "RETURN 1 as n" {} {}'
-        self._vars_v3["#BEGIN_READ#"] = 'BEGIN {"mode": "r"}'
-        self._vars_v3["#BEGIN_WRITE#"] = 'BEGIN {}'
-
-        # Variables for language specifics
-        self._vars_v3["#ADDRESS_STR"] = ''
-        self._vars_v3["#MODE_STR"] = '"mode": "r", "db": "system"'
-
     def setupVersion(self):
         self._database = None
-        self._vars = self._vars_v3
-
-        # Now append language specific variables.
-        if self._driverName in ['dotnet']:
-            self._vars["#ADDRESS_STR"] = "address: " + os.environ.get("TEST_STUB_HOST") + ":#PORT"
-            self._vars["#MODE_STR"] = ''
-            self._vars["#PORT"] = ''
-
+        self._vars = vars_v3
 
