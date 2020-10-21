d634e7697a2444775f0c1694241ae2db348954bf
Peter Wilhelmsson <peter.wilhelmsson@neo4j.com>
2020-10-21 09:05:57 +0200

    Rewrote routing stub tests

--- tests/stub/routing.py
+++ tests/stub/routing.py
@@ -5,141 +5,126 @@ from tests.stub.shared import *
 from nutkit.frontend import Driver, AuthorizationToken
 import nutkit.protocol as types
 
-script_router = """
-!: BOLT #VERSION#
-!: AUTO RESET
-!: AUTO HELLO
-!: AUTO GOODBYE
 
-C: #GET_ROUTING_TABLE#
-C: #PULL_ROUTING_TABLE#
-S: SUCCESS {"fields": ["ttl", "servers"]}
-S: RECORD [1000, [{"addresses": ["#HOST#:9001"], "role":"ROUTE"}, {"addresses": ["#HOST#:9002"], "role":"READ"}, {"addresses": ["#HOST#:9003"], "role":"WRITE"}]]
-S: SUCCESS {"type": "r"}
-"""
-script_read = """
-!: BOLT #VERSION#
-!: AUTO HELLO
-!: AUTO GOODBYE
-!: AUTO RESET
-
-C: #RUN_READ#
-C: #PULL#
-S: SUCCESS {"fields": ["n"]}
-   RECORD [1]
-   SUCCESS {"type": "r"}
-"""
-script_write = """
-!: BOLT #VERSION#
-!: AUTO HELLO
-!: AUTO GOODBYE
-!: AUTO RESET
-
-C: #RUN_WRITE#
-C: #PULL#
-S: SUCCESS {"fields": ["n"]}
-   RECORD [1]
-   SUCCESS {"type": "w"}
-"""
-script_tx_read = """
-!: BOLT #VERSION#
-!: AUTO HELLO
-!: AUTO GOODBYE
-!: AUTO RESET
-
-C: #BEGIN_READ#
-S: SUCCESS {}
-C: RUN "RETURN 1 as n" {} {}
-C: #PULL#
-S: SUCCESS {"fields": ["n"]}
-   SUCCESS {"type": "r"}
-C: COMMIT
-S: SUCCESS {}
-"""
-script_tx_write = """
-!: BOLT #VERSION#
-!: AUTO HELLO
-!: AUTO GOODBYE
-!: AUTO RESET
-
-C: #BEGIN_WRITE#
-S: SUCCESS {}
-C: RUN "RETURN 1 as n" {} {}
-C: #PULL#
-S: SUCCESS {"fields": ["n"]}
-   SUCCESS {"type": "r"}
-C: COMMIT
-S: SUCCESS {}
-   <EXIT>
-"""
-
-
-# Current version of the protocol. Older protocol version should be subclasses to make
-# it easier to remove when no longer supported.
+# This should be the latest/current version of the protocol.
+# Older protocol that needs to be tested inherits from this and override
+# to handle variations.
 class Routing(unittest.TestCase):
-
     def setUp(self):
         self._backend = new_backend()
-        self._driverName = get_driver_name()
-
         self._routingServer = StubServer(9001)
-        # Hook for overriding classes to change script variables
-        self.setupVars()
-        self._routingServer.start(script=script_router, vars=self._vars)
-
         self._readServer = StubServer(9002)
         self._writeServer = StubServer(9003)
-
-        uri = "neo4j://%s" % self._routingServer.address
         # Driver is configured to talk to "routing" stub server
-        self._driver = Driver(self._backend, uri, AuthorizationToken(scheme="basic"))
-
-    def setupVars(self):
-        # Bolt version 4 and later needs named databases when querying for routing table.
-        self._database = 'adb'
-        vars = {
-            'dbName': self._database,
-            # Just the name of the cypher parameter, varies between drivers
-            'dbParamName': 'db',
-            'config': '{"mode": "r", "db": "system"}',
-            # In >= 4.1 an address was added to context to solve issues when setting up routing tables running in docker.
-            # This has been backported to 4.0 in both driver and server since it isn't breaking.
-            'context': '{{"address":"{host}"}}'.format(host=self._routingServer.address),
-            'routes_pull_n': -1,
-        }
-
-
-        # Now append/update language specific variables.
-        if self._driverName in ['dotnet']:
-            vars['dbParamName'] = 'database'
-
-        routing = 'RUN "CALL dbms.routing.getRoutingTable($context, ${dbParamName})" {{"context": {context}, "{dbParamName}":"{dbName}"}} {config}'
-        routing = routing.format(**vars)
-
-        self._vars = {"#VERSION#": '4.0',
-                      "#GET_ROUTING_TABLE#": routing,
-                      "#PULL_ROUTING_TABLE#": 'PULL {{"n": {routes_pull_n}}}'.format(**vars),
-                      "#PULL#": 'PULL {"n": 1000}',
-                      "#RUN_READ#": 'RUN "RETURN 1 as n" {{}} {{"mode": "r", "db": "{dbName}"}}'.format(**vars),
-                      "#RUN_WRITE#": 'RUN "RETURN 1 as n" {{}} {{"db": "{dbName}"}}'.format(**vars),
-                      "#BEGIN_READ#": 'BEGIN {{"mode": "r", "db": "{dbName}"}}'.format(**vars),
-                      "#BEGIN_WRITE#": 'BEGIN {{"db": "{dbName}"}}'.format(**vars),
-                      "#HOST#": self._routingServer.host}
+        uri = "neo4j://%s?region=china&policy=my_policy" % self._routingServer.address
+        self._driver = Driver(self._backend, uri, AuthorizationToken(scheme="basic", principal="p", credentials="c"), userAgent="007")
+        self._routingServer.start(script=self.router_script(), vars=self.get_vars())
 
     def tearDown(self):
-        self._backend.close()
         self._routingServer.reset()
         self._readServer.reset()
         self._writeServer.reset()
 
+    def router_script(self):
+        return """
+        !: BOLT #VERSION#
+        !: AUTO RESET
+        !: AUTO GOODBYE
+
+        C: HELLO {"scheme": "basic", "credentials": "c", "principal": "p", "user_agent": "007", "routing": #ROUTINGCTX#}
+        S: SUCCESS {"server": "Neo4j/4.0.0", "connection_id": "bolt-123456789"}
+        C: RUN "CALL dbms.routing.getRoutingTable($context, $#DBPARAM#)" {"context": #ROUTINGCTX#, "#DBPARAM#": "adb"} {"mode": "r", "db": "system"}
+        C: PULL {"n": -1}
+        S: SUCCESS {"fields": ["ttl", "servers"]}
+        S: RECORD [1000, [{"addresses": ["#HOST#:9001"], "role":"ROUTE"}, {"addresses": ["#HOST#:9002"], "role":"READ"}, {"addresses": ["#HOST#:9003"], "role":"WRITE"}]]
+        S: SUCCESS {"type": "r"}
+        """
+
+    def read_script(self):
+        return """
+        !: BOLT #VERSION#
+        !: AUTO HELLO
+        !: AUTO GOODBYE
+        !: AUTO RESET
+
+        C: RUN "RETURN 1 as n" {} {"mode": "r", "db": "adb"}
+        C: PULL {"n": 1000}
+        S: SUCCESS {"fields": ["n"]}
+           RECORD [1]
+           SUCCESS {"type": "r"}
+        """
+
+    def read_tx_script(self):
+        return """
+        !: BOLT #VERSION#
+        !: AUTO HELLO
+        !: AUTO GOODBYE
+        !: AUTO RESET
+
+        C: BEGIN {"mode": "r", "db": "adb"}
+        S: SUCCESS {}
+        C: RUN "RETURN 1 as n" {} {}
+        C: PULL {"n": 1000}
+        S: SUCCESS {"fields": ["n"]}
+           SUCCESS {"type": "r"}
+        C: COMMIT
+        S: SUCCESS {}
+        """
+
+    def write_script(self):
+        return """
+        !: BOLT #VERSION#
+        !: AUTO HELLO
+        !: AUTO GOODBYE
+        !: AUTO RESET
+
+        C: RUN "RETURN 1 as n" {} {"db": "adb"}
+        C: PULL {"n": 1000}
+        S: SUCCESS {"fields": ["n"]}
+           RECORD [1]
+           SUCCESS {"type": "w"}
+        """
+
+    def write_tx_script(self):
+        return """
+        !: BOLT #VERSION#
+        !: AUTO HELLO
+        !: AUTO GOODBYE
+        !: AUTO RESET
+
+        C: BEGIN {"db": "adb"}
+        S: SUCCESS {}
+        C: RUN "RETURN 1 as n" {} {}
+        C: PULL {"n": 1000}
+        S: SUCCESS {"fields": ["n"]}
+           SUCCESS {"type": "r"}
+        C: COMMIT
+        S: SUCCESS {}
+        """
+
+    def get_vars(self):
+        host = self._routingServer.host
+        dbparam = "db"
+        if get_driver_name() in ['dotnet']:
+            dbparam = "database"
+        return {
+            "#VERSION#": "4.1",
+            "#DBPARAM#": dbparam,
+            "#HOST#": host,
+            "#ROUTINGCTX#": '{"address": "' + host + ':9001", "region": "china", "policy": "my_policy"}',
+        }
+
+    def get_db(self):
+        return "adb"
+
     # Checks that routing is used to connect to correct server and that parameters for
     # session run is passed on to the target server (not the router).
     def test_session_run_read(self):
-        if self._driverName not in ['go', 'dotnet']:
+        if get_driver_name() not in ['go', 'dotnet']:
             self.skipTest("Session with named database not implemented in backend")
 
-        self._readServer.start(script=script_read, vars=self._vars)
-        session = self._driver.session('r', database=self._database)
+        self._readServer.start(script=self.read_script(), vars=self.get_vars())
+        session = self._driver.session('r', database=self.get_db())
         session.run("RETURN 1 as n")
         session.close()
         self._driver.close()
@@ -148,11 +133,11 @@ class Routing(unittest.TestCase):
 
     # Same test as for session.run but for transaction run.
     def test_tx_run_read(self):
-        if self._driverName not in ['go', 'dotnet']:
+        if get_driver_name() not in ['go', 'dotnet']:
             self.skipTest("Session with named database not implemented in backend")
 
-        self._readServer.start(script=script_tx_read, vars=self._vars)
-        session = self._driver.session('r', database=self._database)
+        self._readServer.start(script=self.read_tx_script(), vars=self.get_vars())
+        session = self._driver.session('r', database=self.get_db())
         tx = session.beginTransaction()
         tx.run("RETURN 1 as n")
         tx.commit()
@@ -163,11 +148,11 @@ class Routing(unittest.TestCase):
 
     # Checks that write server is used
     def test_session_run_write(self):
-        if self._driverName not in ['go', 'dotnet']:
+        if get_driver_name() not in ['go', 'dotnet']:
             self.skipTest("Session with named database not implemented in backend")
 
-        self._writeServer.start(script=script_write, vars=self._vars)
-        session = self._driver.session('w', database=self._database)
+        self._writeServer.start(script=self.write_script(), vars=self.get_vars())
+        session = self._driver.session('w', database=self.get_db())
         session.run("RETURN 1 as n")
         session.close()
         self._driver.close()
@@ -176,11 +161,11 @@ class Routing(unittest.TestCase):
 
     # Checks that write server is used
     def test_tx_run_write(self):
-        if self._driverName not in ['go', 'dotnet']:
+        if get_driver_name() not in ['go', 'dotnet']:
             self.skipTest("Session with named database not implemented in backend")
 
-        self._writeServer.start(script=script_tx_write, vars=self._vars)
-        session = self._driver.session('w', database=self._database)
+        self._writeServer.start(script=self.write_tx_script(), vars=self.get_vars())
+        session = self._driver.session('w', database=self.get_db())
         tx = session.beginTransaction()
         tx.run("RETURN 1 as n")
         tx.commit()
@@ -191,32 +176,94 @@ class Routing(unittest.TestCase):
 
 
 class RoutingV3(Routing):
+    def router_script(self):
+        return """
+        !: BOLT #VERSION#
+        !: AUTO RESET
+        !: AUTO GOODBYE
 
-    def setupVars(self, port=None):
-        # Bolt version 4 and later needs named databases when querying for routing table.
-        self._database = None
-        vars = {
-            # In >= 4.1 an address was added to context to solve issues when setting up routing tables running in docker.
-            # This has been backported to all prior protocol versions by some drivers since it is ignored by the database.
-            'context': '{{"address":"{host}"}}'.format(host=self._routingServer.address),
-            'config': '{"mode": "r"}',
+        C: HELLO {"scheme": "basic", "credentials": "c", "principal": "p", "user_agent": "007"}
+        S: SUCCESS {"server": "Neo4j/3.5.0", "connection_id": "bolt-123456789"}
+        C: RUN "CALL dbms.cluster.routing.getRoutingTable($context)" {"context": #ROUTINGCTX#} {#ROUTINGMODE#}
+        C: PULL_ALL
+        S: SUCCESS {"fields": ["ttl", "servers"]}
+        S: RECORD [1000, [{"addresses": ["#HOST#:9001"], "role":"ROUTE"}, {"addresses": ["#HOST#:9002"], "role":"READ"}, {"addresses": ["#HOST#:9003"], "role":"WRITE"}]]
+        S: SUCCESS {"type": "r"}
+        """
+
+    def read_script(self):
+        return """
+        !: BOLT #VERSION#
+        !: AUTO HELLO
+        !: AUTO GOODBYE
+        !: AUTO RESET
+
+        C: RUN "RETURN 1 as n" {} {"mode": "r"}
+        C: PULL_ALL
+        S: SUCCESS {"fields": ["n"]}
+           RECORD [1]
+           SUCCESS {"type": "r"}
+        """
+
+    def read_tx_script(self):
+        return """
+        !: BOLT #VERSION#
+        !: AUTO HELLO
+        !: AUTO GOODBYE
+        !: AUTO RESET
+
+        C: BEGIN {"mode": "r"}
+        S: SUCCESS {}
+        C: RUN "RETURN 1 as n" {} {}
+        C: PULL_ALL
+        S: SUCCESS {"fields": ["n"]}
+           SUCCESS {"type": "r"}
+        C: COMMIT
+        S: SUCCESS {}
+        """
+
+    def write_script(self):
+        return """
+        !: BOLT #VERSION#
+        !: AUTO HELLO
+        !: AUTO GOODBYE
+        !: AUTO RESET
+
+        C: RUN "RETURN 1 as n" {} {}
+        C: PULL_ALL
+        S: SUCCESS {"fields": ["n"]}
+           RECORD [1]
+           SUCCESS {"type": "w"}
+        """
+
+    def write_tx_script(self):
+        return """
+        !: BOLT #VERSION#
+        !: AUTO HELLO
+        !: AUTO GOODBYE
+        !: AUTO RESET
+
+        C: BEGIN {}
+        S: SUCCESS {}
+        C: RUN "RETURN 1 as n" {} {}
+        C: PULL_ALL
+        S: SUCCESS {"fields": ["n"]}
+           SUCCESS {"type": "w"}
+        C: COMMIT
+        S: SUCCESS {}
+        """
+
+    def get_vars(self):
+        host = self._routingServer.host
+        routingMode = ""
+        if get_driver_name() in ['go']:
+            routingMode = '"mode": "r"'
+        return {
+            "#VERSION#": 3,
+            "#HOST#": host,
+            "#ROUTINGMODE#": routingMode,
+            "#ROUTINGCTX#": '{"address": "' + host + ':9001", "region": "china", "policy": "my_policy"}',
         }
 
-        if self._driverName in ['dotnet']:
-            # Actually a 4.1 feature
-            # Request writer when fetching routing table
-            vars['config'] = '{}'
-
-        routing ='RUN "CALL dbms.cluster.routing.getRoutingTable($context)" {{"context": {context}}} {config}'
-        routing = routing.format(**vars)
-
-        self._vars = {"#VERSION#": '3',
-                      "#GET_ROUTING_TABLE#": routing,
-                      "#PULL_ROUTING_TABLE#": 'PULL_ALL',
-                      "#PULL#": 'PULL_ALL',
-                      "#RUN_READ#": 'RUN "RETURN 1 as n" {} {"mode": "r"}',
-                      "#RUN_WRITE#": 'RUN "RETURN 1 as n" {} {}',
-                      "#BEGIN_READ#": 'BEGIN {"mode": "r"}',
-                      "#BEGIN_WRITE#": 'BEGIN {}',
-                      "#HOST#": self._routingServer.host}
-
+    def get_db(self):
+        return None
