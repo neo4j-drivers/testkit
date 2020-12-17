a8732f63a7b2abf95838f86d9b767e26f3f06401
AndyHeap-NeoTech <andrew.heap@neotechnology.com>
2020-12-17 11:56:34 +0000

    Refinement to in_teamcity
    It now handles env variable that contain true, false, 0, 1, is present and is not present. Rather than just present or not.

--- teamcity/__init__.py
+++ teamcity/__init__.py
@@ -3,4 +3,14 @@ from teamcity.download import DockerImage
 from teamcity.testresult import TeamCityTestResult, escape
 
 
-in_teamcity = os.environ.get('TEST_IN_TEAMCITY', False)
+def evaluate_env_variable():
+    env = os.environ.get('TEST_IN_TEAMCITY', 'False').upper()
+    return env == 'TRUE' or env == '1'
+
+
+in_teamcity = evaluate_env_variable()
+
+
+
+
+
