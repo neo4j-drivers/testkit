e232f19bb01ab471a64919c2e728632e6bb087f5
Peter Wilhelmsson <peter.wilhelmsson@neo4j.com>
2020-12-22 10:54:35 +0100

    Python TLS fixes

--- tests/tls/tlsversions.py
+++ tests/tls/tlsversions.py
@@ -1,6 +1,6 @@
 import unittest
-from tests.shared import *
-from tests.tls.shared import *
+from tests.shared import get_driver_name, new_backend
+from tests.tls.shared import TlsServer, try_connect
 
 
 class TestTlsVersions(unittest.TestCase):
@@ -12,20 +12,21 @@ class TestTlsVersions(unittest.TestCase):
 
     def tearDown(self):
         if self._server:
-            # If test raised an exception this will make sure that the stub server
-            # is killed and it's output is dumped for analys.
+            # If test raised an exception this will make sure that the stub
+            # server is killed and it's output is dumped for analys.
             self._server.reset()
             self._server = None
         self._backend.close()
 
     def test_1_1(self):
-        if self._driver in ["dotnet"]:
+        if self._driver in ["dotnet", "python"]:
             self.skipTest("TLS 1.1 is not supported")
 
         self._server = TlsServer("trustedRoot_thehost", minTls="1", maxTls="1")
-        self.assertTrue(try_connect(self._backend, self._server, "neo4j+s", "thehost"))
+        self.assertTrue(try_connect(self._backend, self._server,
+                                    "neo4j+s", "thehost"))
 
     def test_1_2(self):
         self._server = TlsServer("trustedRoot_thehost", minTls="2", maxTls="2")
-        self.assertTrue(try_connect(self._backend, self._server, "neo4j+s", "thehost"))
-
+        self.assertTrue(try_connect(self._backend, self._server,
+                                    "neo4j+s", "thehost"))
