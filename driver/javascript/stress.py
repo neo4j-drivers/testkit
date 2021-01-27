7ff5bb0350940f23c549b31e7e3a63aefd0eb4e4
Antonio Barcelos <antonio.barcelos@neo4j.com>
2021-01-27 15:39:24 +0100

    javascript: Using TEST_NEO4J_STRESS_DURATION as stress test duration config

--- driver/javascript/stress.py
+++ driver/javascript/stress.py
@@ -1,13 +1,11 @@
 import subprocess, os
 
-FIVE_MINUTES = '300'
-
 def run(args):
     subprocess.run(
         args, universal_newlines=True, stderr=subprocess.STDOUT, check=True)
 
 if __name__ == "__main__":
     os.environ['STRESS_TEST_MODE'] = 'fastest'
-    if os.environ.get('TEST_NEO4J_IS_CLUSTER'):
-        os.environ['RUNNING_TIME_IN_SECONDS'] = FIVE_MINUTES
+    os.environ['RUNNING_TIME_IN_SECONDS'] = \
+        os.environ.get('TEST_NEO4J_STRESS_DURATION', 0)
     run(["npm", "run", "run-stress-tests"])
