d550e5643ef185ddc8bb3b14c34a2b129af1f232
Rouven Bauer <rouven.bauer@neotechnology.com>
2021-04-21 08:27:49 +0200

    Code style

--- nutkit/backend/backend.py
+++ nutkit/backend/backend.py
@@ -53,11 +53,11 @@ class Backend:
         self._socket.close()
 
     def send(self, req):
-        reqJson = self._encoder.encode(req)
+        req_json = self._encoder.encode(req)
         if debug:
-            print("Request: %s" % reqJson)
+            print("Request: %s" % req_json)
         self._writer.write("#request begin\n")
-        self._writer.write(reqJson+"\n")
+        self._writer.write(req_json+"\n")
         self._writer.write("#request end\n")
         self._writer.flush()
 
