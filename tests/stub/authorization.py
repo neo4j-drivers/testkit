7de57a687602273182a77ed3e823608ee75e289e
Antonio Barcelos <antonio.barcelos@neo4j.com>
2021-04-30 16:56:28 +0200

    Check connections for closure on LDAP Auth Credential expiration

--- tests/stub/authorization.py
+++ tests/stub/authorization.py
@@ -15,12 +15,34 @@ def get_extra_hello_props():
     return ""
 
 
+class BaseAuthorizationTests(TestkitTestCase):
+    # While there is no unified language agnostic error type mapping, a
+    # dedicated driver mapping is required to determine if the expected
+    # error is returned.
+    def assert_is_authorization_error(self, error):
+        driver = get_driver_name()
+        self.assertEqual("Neo.ClientError.Security.AuthorizationExpired",
+                         error.code)
+        if driver in ['java']:
+            self.assertEqual(
+                'org.neo4j.driver.exceptions.AuthorizationExpiredException',
+                error.errorType)
+        elif driver in ['python']:
+            self.assertEqual(
+                "<class 'neo4j.exceptions.TransientError'>", error.errorType
+            )
+        elif driver in ['javascript']:
+            # only test for code
+            pass
+        else:
+            self.fail("no error mapping is defined for %s driver" % driver)
+
+
 # TODO: find a way to test that driver ditches all open connection in the pool
 #       when encountering Neo.ClientError.Security.AuthorizationExpired
 # TODO: re-write tests, where possible, to use only one server, utilizing
 #       on_send_RetryableNegative and potentially other hooks.
-
-class AuthorizationTests(TestkitTestCase):
+class AuthorizationTests(BaseAuthorizationTests):
     def setUp(self):
         super().setUp()
         self._routing_server1 = StubServer(9000)
@@ -174,27 +196,6 @@ class AuthorizationTests(TestkitTestCase):
 
     def get_db(self):
         return "adb"
-
-    # While there is no unified language agnostic error type mapping, a
-    # dedicated driver mapping is required to determine if the expected
-    # error is returned.
-    def assert_is_authorization_error(self, error):
-        driver = get_driver_name()
-        self.assertEqual("Neo.ClientError.Security.AuthorizationExpired",
-                         error.code)
-        if driver in ['java']:
-            self.assertEqual(
-                'org.neo4j.driver.exceptions.AuthorizationExpiredException',
-                error.errorType)
-        elif driver in ['python']:
-            self.assertEqual(
-                "<class 'neo4j.exceptions.TransientError'>", error.errorType
-            )
-        elif driver in ['javascript']:
-            # only test for code
-            pass
-        else:
-            self.fail("no error mapping is defined for %s driver" % driver)
 
     @staticmethod
     def collectRecords(result):
@@ -712,3 +713,101 @@ class AuthorizationTestsV3(AuthorizationTests):
 
     def get_db(self):
         return None
+
+
+class NoRoutingAuthorizationTests(BaseAuthorizationTests):
+    def setUp(self):
+        super().setUp()
+        self._server = StubServer(9010)
+        self._uri = "bolt://%s:%d" % (self._server.host,
+                                      self._server.port)
+        self._auth = types.AuthorizationToken(
+            scheme="basic", principal="p", credentials="c")
+        self._userAgent = "007"
+
+    def tearDown(self):
+        self._server.reset()
+        super().tearDown()
+
+    def read_return_1_failure_return_2_succeed_script(self):
+        return """
+        !: BOLT 4
+        !: AUTO HELLO
+        !: AUTO GOODBYE
+        !: AUTO RESET
+        !: ALLOW CONCURRENT
+
+        {+
+            {{
+                C: RUN "RETURN 1 as n" {} {"mode": "r"}
+                C: PULL {"n": 1000}
+                S: SUCCESS {"fields": ["n"]}
+                   FAILURE {"code": "Neo.ClientError.Security.AuthorizationExpired", "message": "Authorization expired"}
+                S: <EXIT>
+            ----
+                C: RUN "RETURN 2 as n" {} {"mode": "r"}
+                C: PULL {"n": 1000}
+                S: SUCCESS {"fields": ["n"]}
+                   RECORD [1]
+                   SUCCESS {"type": "r"}
+            }}
+
+        +}
+        """
+
+    def test_should_drop_connection_after_AuthorizationExpired(self):
+        self._server.start(
+            script=self.read_return_1_failure_return_2_succeed_script()
+        )
+        driver = Driver(self._backend, self._uri, self._auth,
+                        userAgent=self._userAgent)
+
+        session1 = driver.session('r')
+        session2 = driver.session('r')
+
+        session1.run('RETURN 2 as n').next()
+
+        try:
+            session2.run('RETURN 1 as n').next()
+        except types.DriverError as e:
+            self.assert_is_authorization_error(e)
+
+        session2.close()
+        session1.close()
+
+        accept_count = self._server.count_responses("<ACCEPT>")
+
+        # fetching another connection and run a query to force
+        # drivers which lazy close the connection do it
+        session3 = driver.session('r')
+        session3.run('RETURN 2 as n').next()
+        session3.close()
+
+        hangup_count = self._server.count_responses("<HANGUP>")
+
+        self.assertEqual(accept_count, hangup_count)
+
+        driver.close()
+
+    def test_should_be_able_to_use_current_sessions_after_AuthorizationExpired(self):
+        self._server.start(
+            script=self.read_return_1_failure_return_2_succeed_script()
+        )
+
+        driver = Driver(self._backend, self._uri, self._auth,
+                        userAgent=self._userAgent)
+
+        session1 = driver.session('r')
+        session2 = driver.session('r')
+
+        session1.run('RETURN 2 as n').next()
+
+        try:
+            session2.run('RETURN 1 as n').next()
+        except types.DriverError as e:
+            self.assert_is_authorization_error(e)
+
+        session2.close()
+
+        session1.run('RETURN 2 as n').next()
+        session1.close()
