00f92bf5e23d34801eeccb210c800a4eade4eb65
Robsdedude <rouven.bauer@neotechnology.com>
2021-07-05 10:49:26 +0200

    Tests for configuration hint connection.recv_timeout_seconds (#157)
    * Adding tests for conf-hint connection_recv_timeout
    * Fix 1st shutdown signal for restarting stub scripts

--- tests/stub/configuration_hints/test_connection_recv_timeout_seconds.py
+++ tests/stub/configuration_hints/test_connection_recv_timeout_seconds.py
@@ -4,12 +4,8 @@ from tests.shared import (
     driver_feature,
     get_driver_name,
     TestkitTestCase,
-    dns_resolve_single,
-    get_dns_resolved_server_address
 )
-from tests.stub.shared import (
-    StubServer,
-)
+from tests.stub.shared import StubServer
 
 
 class TestDirectConnectionRecvTimeout(TestkitTestCase):
@@ -27,16 +23,12 @@ class TestDirectConnectionRecvTimeout(TestkitTestCase):
         # If test raised an exception this will make sure that the stub server
         # is killed and it's output is dumped for analysis.
         self._server.reset()
-        if self._session:
-            self._session.close()
-        if self._driver:
-            self._driver.close()
         super().tearDown()
 
     def _start_server(self, script):
         self._server.start(path=self.script_path(script))
 
-    def _assert_is_timout_exception(self, e):
+    def _assert_it_timout_exception(self, e):
         if get_driver_name() in ["python"]:
             self.assertEqual(e.errorType,
                              "<class 'neo4j.exceptions.ServiceUnavailable'>")
@@ -48,31 +40,24 @@ class TestDirectConnectionRecvTimeout(TestkitTestCase):
     def test_timeout(self):
         self._start_server("1_second_exceeds.script")
         with self.assertRaises(types.DriverError) as exc:
-            self._session.run("timeout")
-        self._session.run("in time")
-        self._server.done()
-        self._assert_is_timout_exception(exc.exception)
-        self.assertEqual(self._server.count_responses("<ACCEPT>"), 2)
-        self.assertEqual(self._server.count_responses("<HANGUP>"), 2)
-        self.assertEqual(self._server.count_requests('RUN "timeout"'), 1)
-        self.assertEqual(self._server.count_requests('RUN "in time"'), 1)
+            self._session.run("RETURN 1 AS n")
+        self._assert_it_timout_exception(exc.exception)
+        response_after_sleep = bool(
+            self._server.count_responses('SUCCESS {"type": "r"}')
+        )
+        self.assertFalse(response_after_sleep)
 
     @driver_feature(types.Feature.CONF_HINT_CON_RECV_TIMEOUT)
     def test_timeout_unmanaged_tx(self):
         self._start_server("1_second_exceeds_tx.script")
         tx = self._session.beginTransaction()
         with self.assertRaises(types.DriverError) as exc:
-            tx.run("timeout")
-        tx = self._session.beginTransaction()
-        res = tx.run("in time")
-        res.next()
-        tx.commit()
-        self._server.done()
-        self._assert_is_timout_exception(exc.exception)
-        self.assertEqual(self._server.count_responses("<ACCEPT>"), 2)
-        self.assertEqual(self._server.count_responses("<HANGUP>"), 2)
-        self.assertEqual(self._server.count_requests('RUN "timeout"'), 1)
-        self.assertEqual(self._server.count_requests('RUN "in time"'), 1)
+            tx.run("RETURN 1 AS n")
+        self._assert_it_timout_exception(exc.exception)
+        response_after_sleep = bool(
+            self._server.count_responses('SUCCESS {"type": "r"}')
+        )
+        self.assertFalse(response_after_sleep)
 
     @driver_feature(types.Feature.CONF_HINT_CON_RECV_TIMEOUT)
     def test_timeout_managed_tx_retry(self):
@@ -86,7 +71,7 @@ class TestDirectConnectionRecvTimeout(TestkitTestCase):
             if retries == 1:
                 with self.assertRaises(types.DriverError) as exc:
                     tx.run("RETURN 1 AS n")
-                self._assert_is_timout_exception(exc.exception)
+                self._assert_it_timout_exception(exc.exception)
                 self._on_failed_retry_assertions()
                 raise exc.exception
             result = tx.run("RETURN %i AS n" % retries)
@@ -95,25 +80,22 @@ class TestDirectConnectionRecvTimeout(TestkitTestCase):
 
         self._start_server("1_second_exceeds_tx_retry.script")
         self._session.writeTransaction(work)
-        self._server.done()
+        run_success_count = \
+            self._server.count_responses('SUCCESS {"type": "r"}')
         self.assertEqual(retries, 2)
+        self.assertEqual(run_success_count, 1)
         self.assertIsInstance(record, types.Record)
         self.assertEqual(record.values, [types.CypherInt(1)])
-        self.assertEqual(self._server.count_responses("<ACCEPT>"), 2)
-        self.assertEqual(self._server.count_responses("<HANGUP>"), 2)
-        self.assertEqual(self._server.count_responses("<BROKEN>"), 1)
 
     @driver_feature(types.Feature.CONF_HINT_CON_RECV_TIMEOUT)
     def test_in_time(self):
         self._start_server("2_seconds_in_time.script")
         result = self._session.run("RETURN 1 AS n")
         record = result.next()
-        self.assertIsInstance(result.next(), types.NullRecord)
-        self._server.done()
         self.assertIsInstance(record, types.Record)
         self.assertEqual(record.values, [types.CypherInt(1)])
-        self.assertEqual(self._server.count_responses("<ACCEPT>"), 1)
-        self.assertEqual(self._server.count_responses("<BROKEN>"), 0)
+        self.assertIsInstance(result.next(), types.NullRecord)
+        self._server.done()
 
     @driver_feature(types.Feature.CONF_HINT_CON_RECV_TIMEOUT)
     def test_in_time_unmanaged_tx(self):
@@ -121,13 +103,10 @@ class TestDirectConnectionRecvTimeout(TestkitTestCase):
         tx = self._session.beginTransaction()
         result = tx.run("RETURN 1 AS n")
         record = result.next()
-        tx.commit()
-        self.assertIsInstance(result.next(), types.NullRecord)
-        self._server.done()
         self.assertIsInstance(record, types.Record)
         self.assertEqual(record.values, [types.CypherInt(1)])
-        self.assertEqual(self._server.count_responses("<ACCEPT>"), 1)
-        self.assertEqual(self._server.count_responses("<BROKEN>"), 0)
+        self.assertIsInstance(result.next(), types.NullRecord)
+        self._server.done()
 
     @driver_feature(types.Feature.CONF_HINT_CON_RECV_TIMEOUT)
     def test_in_time_managed_tx_retry(self):
@@ -138,18 +117,16 @@ class TestDirectConnectionRecvTimeout(TestkitTestCase):
             nonlocal retries
             nonlocal record
             retries += 1
-            result = tx.run("RETURN 1 AS n")
+            result = tx.run("RETURN %i AS n" % retries)
             record = result.next()
             self.assertIsInstance(result.next(), types.NullRecord)
 
         self._start_server("2_seconds_in_time_tx_retry.script")
         self._session.writeTransaction(work)
-        self._server.done()
         self.assertEqual(retries, 1)
         self.assertIsInstance(record, types.Record)
         self.assertEqual(record.values, [types.CypherInt(1)])
-        self.assertEqual(self._server.count_responses("<ACCEPT>"), 1)
-        self.assertEqual(self._server.count_responses("<BROKEN>"), 0)
+        self._server.done()
 
 
 class TestRoutingConnectionRecvTimeout(TestDirectConnectionRecvTimeout):
@@ -157,9 +134,8 @@ class TestRoutingConnectionRecvTimeout(TestDirectConnectionRecvTimeout):
         super().setUp()
         self._server = StubServer(9010)
         self._router = StubServer(9000)
-        self._router.start(path=self.script_path("router.script"), vars={
-            "#HOST#": dns_resolve_single(self._router.host)
-        })
+        self._router.start(path=self.script_path("router.script"),
+                           vars={"#HOST#": self._router.host})
         auth = types.AuthorizationToken(scheme="basic", principal="neo4j",
                                         credentials="pass")
         uri = "neo4j://%s" % self._router.address
@@ -170,21 +146,20 @@ class TestRoutingConnectionRecvTimeout(TestDirectConnectionRecvTimeout):
     def tearDown(self):
         # If test raised an exception this will make sure that the stub server
         # is killed and it's output is dumped for analysis.
+        self._server.reset()
         self._router.reset()
         super().tearDown()
 
-    def _assert_is_timout_exception(self, e):
+    def _assert_it_timout_exception(self, e):
         if get_driver_name() in ["python"]:
             self.assertEqual(e.errorType,
                              "<class 'neo4j.exceptions.SessionExpired'>")
         else:
-            super()._assert_is_timout_exception(e)
+            super()._assert_it_timout_exception(e)
 
     def _on_failed_retry_assertions(self):
         rt = self._driver.getRoutingTable()
-        self.assertEqual(rt.routers, [
-            get_dns_resolved_server_address(self._router)
-        ])
+        self.assertEqual(rt.routers, [self._router.address])
         self.assertEqual(rt.readers, [])
         self.assertEqual(rt.writers, [])
 
@@ -192,23 +167,21 @@ class TestRoutingConnectionRecvTimeout(TestDirectConnectionRecvTimeout):
         self.assertEqual(self._router.count_responses("<HANGUP>"), 0)
         self._router.done()
         self._server.reset()
-        if timed_out:
+        if timed_out and managed:
             self.assertEqual(self._server.count_responses("<ACCEPT>"), 2)
             self.assertEqual(self._router.count_requests("ROUTE"), 2)
         else:
-            self.assertEqual(self._server.count_responses("<ACCEPT>"), 1)
             self.assertEqual(self._router.count_requests("ROUTE"), 1)
+            self.assertEqual(self._server.count_responses("<ACCEPT>"), 1)
         self.assertEqual(self._router.count_responses("<ACCEPT>"), 1)
         rt = self._driver.getRoutingTable()
-        self.assertEqual(rt.routers, [
-            get_dns_resolved_server_address(self._router)
-        ])
-        self.assertEqual(rt.readers, [
-            get_dns_resolved_server_address(self._server)
-        ])
-        self.assertEqual(rt.writers, [
-            get_dns_resolved_server_address(self._server)
-        ])
+        self.assertEqual(rt.routers, [self._router.address])
+        if timed_out and not managed:
+            self.assertEqual(rt.readers, [])
+            self.assertEqual(rt.writers, [])
+        else:
+            self.assertEqual(rt.readers, [self._server.address])
+            self.assertEqual(rt.writers, [self._server.address])
 
     def test_timeout(self):
         super().test_timeout()
