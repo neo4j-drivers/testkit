cbb88fa7206fab5d25d0710fe4f2d64c68f21255
Rouven Bauer <rouven.bauer@neo4j.com>
2021-08-03 09:34:19 +0200

    Improve OPT_MINIMAL_RESETS test coverage

--- tests/stub/optimizations/test_optimizations.py
+++ tests/stub/optimizations/test_optimizations.py
@@ -1,3 +1,5 @@
+import re
+
 from nutkit.frontend import Driver
 import nutkit.protocol as types
 
@@ -25,7 +27,7 @@ class TestOptimizations(TestkitTestCase):
     def test_pull_pipelining(self):
         def test():
             script = "pull_pipeline{}.script".format("_tx" if use_tx else "")
-            self._server.start(path=self.script_path(script),
+            self._server.start(path=self.script_path("v4x3", script),
                                vars={"#TYPE#": mode[0]})
             auth = types.AuthorizationToken(scheme="basic", principal="neo4j",
                                             credentials="pass")
@@ -54,12 +56,15 @@ class TestOptimizations(TestkitTestCase):
                 self._server.reset()
 
     def double_read(self, mode, new_session, use_tx, routing,
-                    check_single_connection, check_no_reset):
-        script = "run_twice{}.script".format("_tx" if use_tx else "")
-        self._server.start(path=self.script_path(script),
+                    check_single_connection, check_no_reset, version="v4x3",
+                    consume=False):
+        assert use_tx in (None, "commit", "rollback")
+        script = "run_twice{}{}.script".format("_tx" if use_tx else "",
+                                               "_discard" if consume else "")
+        self._server.start(path=self.script_path(version, script),
                            vars={"#TYPE#": mode[0]})
         if routing:
-            self._router.start(path=self.script_path("router.script"),
+            self._router.start(path=self.script_path("v4x3", "router.script"),
                                vars={"#HOST#": self._router.host})
         auth = types.AuthorizationToken(scheme="basic", principal="neo4j",
                                         credentials="pass")
@@ -80,34 +85,60 @@ class TestOptimizations(TestkitTestCase):
             if use_tx:
                 tx = session.beginTransaction()
                 res = tx.run("QUERY %i" % (i + 1))
-                results.append(
-                    list(map(lambda r: r.value, res.next().values))
-                )
-                tx.commit()
+                if consume:
+                    res.consume()
+                else:
+                    results.append(
+                        list(map(lambda r: r.value, res.next().values))
+                    )
+                if use_tx == "commit":
+                    tx.commit()
+                else:
+                    tx.rollback()
             else:
                 res = session.run("QUERY %i" % (i + 1))
-                results.append(
-                    list(map(lambda r: r.value, res.next().values))
-                )
+                if consume:
+                    res.consume()
+                else:
+                    results.append(
+                        list(map(lambda r: r.value, res.next().values))
+                    )
 
         session.close()
         driver.close()
-        self.assertEqual(results, [[1], [2]])
+        discard_count = \
+            self._server.count_requests(re.compile(r"DISCARD(_ALL)?"))
+        if use_tx:
+            commit_count = self._server.count_requests("COMMIT")
+            rollback_count = self._server.count_requests("ROLLBACK")
+            if use_tx == "commit":
+                self.assertEqual(commit_count, 2)
+                self.assertEqual(rollback_count, 0)
+            else:
+                self.assertEqual(commit_count, 0)
+                self.assertEqual(rollback_count, 2)
+        if consume:
+            self.assertEqual(discard_count, 2)
+        else:
+            self.assertEqual(results, [[1], [2]])
+            self.assertEqual(discard_count, 0)
         self._server.done()
         self._router.reset()
         if check_single_connection:
             self.assertEqual(self._server.count_responses("<ACCEPT>"), 1)
         if check_no_reset:
             self.assertEqual(self._server.count_requests("RESET"), 0)
+            self.assertEqual(self._router.count_requests("RESET"), 0)
 
     @driver_feature(types.Feature.OPT_CONNECTION_REUSE)
     def test_reuses_connection(self):
         for routing in (False, True):
             for mode in ("read", "write"):
-                for use_tx in (True, False):
+                for use_tx in (None, "commit", "rollback"):
                     for new_session in (True, False):
                         with self.subTest(mode
-                                          + ("_tx" if use_tx else "")
+                                          + (("_tx_" + use_tx) if use_tx
+                                             else "")
                                           + ("_one_shot_session" if new_session
                                              else "_reuse_session")
                                           + ("_routing" if routing
@@ -119,20 +150,34 @@ class TestOptimizations(TestkitTestCase):
     @driver_feature(types.Feature.OPT_MINIMAL_RESETS)
     def test_no_reset_on_clean_connection(self):
         mode = "write"
-        for use_tx in (True, False):
-            for new_session in (True, False):
-                with self.subTest(mode
-                                  + ("_tx" if use_tx else "")
-                                  + ("_one_shot_session" if new_session
-                                     else"_reuse_session")):
-                    self.double_read(mode, new_session, use_tx, False,
-                                     False, True)
-                self._server.reset()
+        for version in ("v4x3", "v3"):
+            for consume in (True, False):
+                if version == "v3" and consume:
+                    # Drivers with types.Feature.OPT_PULL_PIPELINING will issue
+                    # PULL_ALL, so there is nothing left to DISCARD. Since this
+                    # is an old protocol version anyway, TestKit does not offer
+                    # test coverage for this case.
+                    continue
+                for use_tx in (None, "commit", "rollback"):
+                    for new_session in (True, False):
+                        with self.subTest(mode
+                                          + "_" + version
+                                          + (("_tx_" + use_tx) if use_tx
+                                             else "")
+                                          + ("_one_shot_session" if new_session
+                                             else"_reuse_session")
+                                          + ("_discard" if consume
+                                             else "_pull")):
+                            self.double_read(mode, new_session, use_tx, False,
+                                             False, True, version=version,
+                                             consume=consume)
+                        self._server.reset()
 
     @driver_feature(types.Feature.OPT_IMPLICIT_DEFAULT_ARGUMENTS)
     def test_uses_implicit_default_arguments(self):
         def test():
-            self._server.start(path=self.script_path("all_default.script"))
+            self._server.start(path=self.script_path("v4x3",
+                                                     "all_default.script"))
             auth = types.AuthorizationToken(scheme="basic", principal="neo4j",
                                             credentials="pass")
             driver = Driver(self._backend, "bolt://%s" % self._server.address,
@@ -141,17 +186,25 @@ class TestOptimizations(TestkitTestCase):
             if use_tx:
                 tx = session.beginTransaction()
                 res = tx.run("CYPHER")
-                res.next()
+                if consume:
+                    res.consume()
+                else:
+                    res.next()
                 tx.commit()
             else:
                 res = session.run("CYPHER")
-                res.next()
+                if consume:
+                    res.consume()
+                else:
+                    res.next()
 
             session.close()
             driver.close()
             self._server.done()
 
         for use_tx in (True, False):
-            with self.subTest("tx" if use_tx else "auto_commit"):
-                test()
-            self._server.reset()
+            for consume in (True, False):
+                with self.subTest(("tx" if use_tx else "auto_commit")
+                                  + ("_discard" if consume else "_pull")):
+                    test()
+                self._server.reset()
