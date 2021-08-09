8030e9d82f11e1cc4b4569eeab374508dd3720a4
Dmitriy Tverdiakov <11927660+injectives@users.noreply.github.com>
2021-08-09 17:09:52 +0100

    Stub tests migration from java driver (#200)
    Migrated tests:
    - shouldBeAbleRunCypher -> test_should_read_successfully_using_write_session_run
    - shouldSendMultipleBookmarks -> test_send_and_receive_multiple_bookmarks_write_tx
    - shouldSendNullRoutingContextForBoltUri -> test_should_exclude_routing_context (optimization)
    - shouldSendReadAccessModeInQueryMetadata -> test_should_read_successfully_using_read_session_run
    - shouldNotSendWriteAccessModeInQueryMetadata -> test_should_read_successfully_using_write_session_run
    - shouldSendCustomerUserAgentInHelloMessage -> test_should_send_custom_user_agent_using_write_session_run

---
+++ tests/stub/routing/test_no_routing_v3.py
@@ -0,0 +1,21 @@
+from tests.shared import (
+    get_driver_name,
+)
+from ._routing import get_extra_hello_props
+from .test_no_routing_v4x1 import NoRoutingV4x1
+
+
+class NoRoutingV3(NoRoutingV4x1):
+    bolt_version = "3"
+    version_dir = "v3_no_routing"
+    server_agent = "Neo4j/3.5.0"
+    adb = None
+
+    def get_vars(self):
+        return {
+            "#VERSION#": "3",
+            "#SERVER_AGENT#": "Neo4j/3.5.0",
+            "#EXTRA_HELLO_PROPS#": get_extra_hello_props(),
+            "#USER_AGENT#": '007',
+            "#ROUTING#": ''
+        }
