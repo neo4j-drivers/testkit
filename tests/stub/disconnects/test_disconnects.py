06d0938c6b2854848c684297342fd099ddc05515
Dmitriy Tverdiakov <11927660+injectives@users.noreply.github.com>
2021-08-09 17:47:49 +0100

    Remove redundant null routing for java driver (#203)

--- tests/stub/disconnects/test_disconnects.py
+++ tests/stub/disconnects/test_disconnects.py
@@ -287,7 +287,7 @@ class TestDisconnects(TestkitTestCase):
         if self._driverName == "javascript":
             return ', "realm": "", "ticket": ""'
         elif self._driverName == "java":
-            return ', "realm": "", "routing": null'
+            return ', "realm": ""'
         elif self._driverName == "dotnet":
             return ', "routing": null'
         else:
