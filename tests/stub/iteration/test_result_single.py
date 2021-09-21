29f4a4a130f4e45bb4325642762b6572f9b753c1
Rouven Bauer <rouven.bauer@neo4j.com>
2021-09-21 11:13:57 +0200

    Fix mismatched feature flags and file names

--- tests/stub/iteration/test_result_single.py
+++ tests/stub/iteration/test_result_single.py
@@ -19,6 +19,15 @@ class TestResultSingle(TestkitTestCase):
         self._server.reset()
         super().tearDown()
 
+    def _assert_not_exactly_one_record_error(self, error):
+        self.assertIsInstance(error, types.DriverError)
+        driver = get_driver_name()
+        if driver in ["python"]:
+            self.assertEqual("<class 'ToBeDecided'>",
+                             error.errorType)
+        else:
+            self.fail("no error mapping is defined for %s driver" % driver)
+
     def _assert_connection_error(self, error):
         self.assertIsInstance(error, types.DriverError)
         driver = get_driver_name()
@@ -44,66 +53,56 @@ class TestResultSingle(TestkitTestCase):
             self._server.reset()
 
     @driver_feature(types.Feature.API_RESULT_SINGLE)
-    def test_result_peek_with_0_records(self):
+    def test_result_single_with_0_records(self):
         with self._session("yield_0_records.script") as session:
             result = session.run("RETURN 1 AS n")
-            record = result.peek()
-            self.assertIsInstance(record, types.NullRecord)
+            with self.assertRaises(types.DriverError) as exc:
+                result.single()
+            self._assert_not_exactly_one_record_error(exc.exception)
 
     @driver_feature(types.Feature.API_RESULT_SINGLE)
-    def test_result_peek_with_1_records(self):
+    def test_result_single_with_1_records(self):
         with self._session("yield_1_record.script") as session:
             result = session.run("RETURN 1 AS n")
-            record = result.peek()
+            record = result.single()
             self.assertIsInstance(record, types.Record)
             self.assertEqual(record.values, [types.CypherInt(1)])
-            record = result.next()
-            self.assertIsInstance(record, types.Record)
-            self.assertEqual(record.values, [types.CypherInt(1)])
-            record = result.peek()
-            self.assertIsInstance(record, types.NullRecord)
 
     @driver_feature(types.Feature.API_RESULT_SINGLE)
-    def test_result_peek_with_2_records(self):
+    def test_result_single_with_2_records(self):
         def _test():
             with self._session("yield_2_records.script",
                                fetch_size=fetch_size) as session:
                 result = session.run("RETURN 1 AS n")
-                for i in (1, 2):
-                    record = result.peek()
-                    self.assertIsInstance(record, types.Record)
-                    self.assertEqual(record.values, [types.CypherInt(i)])
-                    record = result.next()
-                    self.assertIsInstance(record, types.Record)
-                    self.assertEqual(record.values, [types.CypherInt(i)])
-                record = result.peek()
-                self.assertIsInstance(record, types.NullRecord)
+                with self.assertRaises(types.DriverError) as exc:
+                    result.single()
+                self._assert_not_exactly_one_record_error(exc.exception)
 
         for fetch_size in (1, 2):
             with self.subTest("fetch_size-%i" % fetch_size):
                 _test()
 
     @driver_feature(types.Feature.API_RESULT_SINGLE)
-    def test_result_peek_with_disconnect(self):
+    def test_result_single_with_disconnect(self):
         with self._session("disconnect_on_pull.script") as session:
             result = session.run("RETURN 1 AS n")
             with self.assertRaises(types.DriverError) as exc:
-                result.peek()
+                result.single()
             self._assert_connection_error(exc.exception)
 
     @driver_feature(types.Feature.API_RESULT_SINGLE)
-    def test_result_peek_with_failure(self):
+    def test_result_single_with_failure(self):
         err = "Neo.TransientError.Completely.MadeUp"
 
         with self._session("error_on_pull.script",
                            vars_={"#ERROR#": err}) as session:
             result = session.run("RETURN 1 AS n")
             with self.assertRaises(types.DriverError) as exc:
-                result.peek()
+                result.single()
             self.assertEqual(err, exc.exception.code)
 
     @driver_feature(types.Feature.API_RESULT_SINGLE)
-    def test_result_peek_with_failure_tx_run(self):
+    def test_result_single_with_failure_tx_run(self):
         err = "Neo.TransientError.Completely.MadeUp"
 
         with self._session("tx_error_on_pull.script",
@@ -111,11 +110,11 @@ class TestResultSingle(TestkitTestCase):
             tx = session.beginTransaction()
             result = tx.run("RETURN 1 AS n")
             with self.assertRaises(types.DriverError) as exc:
-                result.peek()
+                result.single()
             self.assertEqual(err, exc.exception.code)
 
     @driver_feature(types.Feature.API_RESULT_SINGLE)
-    def test_result_peek_with_failure_tx_func_run(self):
+    def test_result_single_with_failure_tx_func_run(self):
         err = "Neo.TransientError.Completely.MadeUp"
         work_call_count = 0
 
@@ -125,11 +124,11 @@ class TestResultSingle(TestkitTestCase):
             result = tx.run("RETURN 1 AS n")
             if work_call_count == 1:
                 with self.assertRaises(types.DriverError) as exc:
-                    result.peek()
+                    result.single()
                 self.assertEqual(err, exc.exception.code)
                 raise exc.exception
             else:
-                return result.peek()
+                return result.single()
 
         with self._session("tx_error_on_pull.script",
                            vars_={"#ERROR#": err}) as session:
