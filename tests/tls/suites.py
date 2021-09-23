d57b5283f9f6c037ed2bb19fdb0994ef86278cc5
Rouven Bauer <rouven.bauer@neo4j.com>
2021-09-23 16:51:15 +0200

    Rename tls test modules

--- tests/tls/suites.py
+++ tests/tls/suites.py
@@ -10,24 +10,25 @@ from tests.testenv import (
     end_test_suite,
     get_test_result_class,
 )
-import tests.tls.securescheme as securescheme
-import tests.tls.selfsignedscheme as selfsignedscheme
-import tests.tls.tlsversions as tlsversions
-import tests.tls.unsecurescheme as unsecurescheme
+from tests.tls import test_secure_scheme
+from tests.tls import test_self_signed_scheme
+from tests.tls import test_tls_versions
+from tests.tls import test_unsecure_scheme
 
 
 loader = unittest.TestLoader()
 
 tls_suite = unittest.TestSuite()
-tls_suite.addTests(loader.loadTestsFromModule(securescheme))
-tls_suite.addTests(loader.loadTestsFromModule(selfsignedscheme))
-tls_suite.addTests(loader.loadTestsFromModule(unsecurescheme))
-tls_suite.addTests(loader.loadTestsFromModule(tlsversions))
+tls_suite.addTests(loader.loadTestsFromModule(test_secure_scheme))
+tls_suite.addTests(loader.loadTestsFromModule(test_self_signed_scheme))
+tls_suite.addTests(loader.loadTestsFromModule(test_tls_versions))
+tls_suite.addTests(loader.loadTestsFromModule(test_unsecure_scheme))
 
 if __name__ == "__main__":
     suiteName = "TLS tests"
     begin_test_suite(suiteName)
-    runner = unittest.TextTestRunner(resultclass=get_test_result_class(), verbosity=100)
+    runner = unittest.TextTestRunner(resultclass=get_test_result_class(),
+                                     verbosity=100)
     result = runner.run(tls_suite)
     end_test_suite(suiteName)
     if result.errors or result.failures:
