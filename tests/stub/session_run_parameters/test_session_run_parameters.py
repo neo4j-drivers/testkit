9f95674000c7362d7768d03536d11a27fc309749
Rouven Bauer <rouven.bauer@neo4j.com>
2021-10-06 16:52:31 +0200

    Add impersonation and explicit db tests to session run and tx run
    Also extended the tests to be run with and without routing.

--- tests/stub/session_run_parameters/test_session_run_parameters.py
+++ tests/stub/session_run_parameters/test_session_run_parameters.py
@@ -19,20 +19,46 @@ from tests.stub.shared import StubServer
 class TestSessionRunParameters(TestkitTestCase):
     def setUp(self):
         super().setUp()
+        self._router = StubServer(9000)
         self._server = StubServer(9001)
         self._driverName = get_driver_name()
-        uri = "bolt://%s" % self._server.address
-        self._driver = Driver(self._backend, uri,
-                              types.AuthorizationToken("basic", principal="",
-                                                       credentials=""))
 
     def tearDown(self):
         # If test raised an exception this will make sure that the stub server
         # is killed and it's output is dumped for analysis.
         self._server.reset()
+        self._router.reset()
         super().tearDown()
 
-    def _run(self, session_args=None, session_kwargs=None,
+    def _start_servers_and_driver(self, script, routing, db, impersonation):
+        if routing:
+            router_script = "router%s%%s.script"
+            if db:
+                router_script = router_script % ("_" + db)
+            else:
+                router_script = router_script % "_default_db"
+            if impersonation:
+                router_script = router_script % "_impersonation"
+            else:
+                router_script = router_script % ""
+            print("router script:", router_script)
+            self._router.start(path=self.script_path(router_script),
+                               vars={"#HOST#": self._router.host})
+        if routing and not db:
+            script += "_homedb.script"
+        else:
+            script += ".script"
+        print("server script:", script)
+        self._server.start(path=self.script_path(script))
+        if routing:
+            uri = "neo4j://%s" % self._router.address
+        else:
+            uri = "bolt://%s" % self._server.address
+        self._driver = Driver(self._backend, uri,
+                              types.AuthorizationToken("basic", principal="",
+                                                       credentials=""))
+
+    def _run(self, script, routing, session_args=None, session_kwargs=None,
              run_args=None, run_kwargs=None):
         if session_args is None:
             session_args = ()
@@ -42,85 +68,103 @@ class TestSessionRunParameters(TestkitTestCase):
             run_args = ()
         if run_kwargs is None:
             run_kwargs = {}
+        self._start_servers_and_driver(script, routing,
+                                       session_kwargs.get("database"),
+                                       session_kwargs.get("impersonatedUser"))
         session = self._driver.session(*session_args, **session_kwargs)
         try:
             result = session.run("RETURN 1 as n", *run_args, **run_kwargs)
             result.next()
+            self._server.done()
         finally:
+            self._server.reset()
+            self._router.reset()
             session.close()
-
-    def _start_server(self, script):
-        self._server.start(path=self.script_path(script))
+            self._driver.close()
 
     def test_access_mode_read(self):
-        self._start_server("access_mode_read.script")
-        self._run(session_args=("r",))
-        self._driver.close()
-        self._server.done()
+        for routing in (True, False):
+            with self.subTest("routing" if routing else "direct"):
+                self._run("access_mode_read", routing,
+                          session_args=("r",))
 
     def test_access_mode_write(self):
-        self._start_server("access_mode_write.script")
-        self._run(session_args=("w",))
-        self._driver.close()
-        self._server.done()
+        for routing in (True, False):
+            with self.subTest("routing" if routing else "direct"):
+                self._run("access_mode_write", routing,
+                          session_args=("w",))
 
     def test_parameters(self):
-        self._start_server("parameters.script")
-        self._run(session_args=("w",),
-                  run_kwargs={"params": {"p": types.CypherInt(1)}})
-        self._driver.close()
-        self._server.done()
+        for routing in (True, False):
+            with self.subTest("routing" if routing else "direct"):
+                self._run("parameters", routing,
+                          session_args=("w",),
+                          run_kwargs={"params": {"p": types.CypherInt(1)}})
 
     def test_bookmarks(self):
-        self._start_server("bookmarks.script")
-        self._run(session_args=("w",),
-                  session_kwargs={"bookmarks": ["b1", "b2"]})
-        self._driver.close()
-        self._server.done()
+        for routing in (True, False):
+            with self.subTest("routing" if routing else "direct"):
+                self._run("bookmarks", routing,
+                          session_args=("w",),
+                          session_kwargs={"bookmarks": ["b1", "b2"]})
 
     def test_tx_meta(self):
-        self._start_server("tx_meta.script")
-        self._run(session_args=("w",),
-                  run_kwargs={"txMeta": {"akey": "aval"}})
-        self._driver.close()
-        self._server.done()
+        for routing in (True, False):
+            with self.subTest("routing" if routing else "direct"):
+                self._run("tx_meta", routing,
+                          session_args=("w",),
+                          run_kwargs={"txMeta": {"akey": "aval"}})
 
     def test_timeout(self):
-        self._start_server("timeout.script")
-        self._run(session_args=("w",), run_kwargs={"timeout": 17})
-        self._driver.close()
-        self._server.done()
+        for routing in (True, False):
+            with self.subTest("routing" if routing else "direct"):
+                self._run("timeout", routing,
+                          session_args=("w",), run_kwargs={"timeout": 17})
+
+    def test_database(self):
+        for routing in (True, False):
+            with self.subTest("routing" if routing else "direct"):
+                self._run("adb", routing,
+                          session_args=("w",),
+                          session_kwargs={"database": "adb"})
 
     @driver_feature(types.Feature.IMPERSONATION)
     def test_impersonation(self):
-        self._start_server("imp_user.script")
-        self._run(session_args=("w",),
-                  session_kwargs={"impersonatedUser": "that-other-dude"})
-        self._driver.close()
-        self._server.done()
+        for routing in (True, False):
+            with self.subTest("routing" if routing else "direct"):
+                self._run("imp_user", routing,
+                          session_args=("w",),
+                          session_kwargs={
+                              "impersonatedUser": "that-other-dude"
+                          })
 
     @driver_feature(types.Feature.IMPERSONATION)
     def test_combined(self):
-        self._start_server("combined.script")
-        self._run(session_args=("r",),
-                  session_kwargs={
-                      "bookmarks": ["b0"],
-                      "impersonatedUser": "that-other-dude"
-                  },
-                  run_kwargs={
-                      "params": {"p": types.CypherInt(1)},
-                      "txMeta": {"k": "v"}, "timeout": 11
-                  })
-        self._driver.close()
-        self._server.done()
+        for routing in (True, False):
+            with self.subTest("routing" if routing else "direct"):
+                self._run("combined", routing,
+                          session_args=("r",),
+                          session_kwargs={
+                              "bookmarks": ["b0"],
+                              "database": "adb",
+                              "impersonatedUser": "that-other-dude"
+                          },
+                          run_kwargs={
+                              "params": {"p": types.CypherInt(1)},
+                              "txMeta": {"k": "v"}, "timeout": 11
+                          })
 
     def test_empty_query(self):
+        # Driver should pass empty string to server
+        # TODO remove this block once all languages work
         if get_driver_name() in ["javascript", "java"]:
             self.skipTest("rejects empty string")
-        # Driver should pass empty string to server
-        self._start_server("empty_query.script")
-        session = self._driver.session("w")
-        session.run("").next()
-        session.close()
-        self._driver.close()
-        self._server.done()
+        for routing in (True, False):
+            with self.subTest("routing" if routing else "direct"):
+                self._start_servers_and_driver("empty_query", routing,
+                                               None, None)
+                session = self._driver.session("w")
+                session.run("").next()
+                session.close()
+                self._driver.close()
+                self._server.done()
--- tests/stub/tx_begin_parameters/test_tx_begin_parameters.py
+++ tests/stub/tx_begin_parameters/test_tx_begin_parameters.py
@@ -19,21 +19,46 @@ from tests.stub.shared import StubServer
 class TestTxBeginParameters(TestkitTestCase):
     def setUp(self):
         super().setUp()
+        self._router = StubServer(9000)
         self._server = StubServer(9001)
         self._driverName = get_driver_name()
-        uri = "bolt://%s" % self._server.address
-        self._driver = Driver(self._backend, uri,
-                              types.AuthorizationToken("basic", principal="",
-                                                       credentials=""))
 
     def tearDown(self):
         # If test raised an exception this will make sure that the stub server
         # is killed and it's output is dumped for analysis.
-        self._driver.close()
         self._server.reset()
+        self._router.reset()
         super().tearDown()
 
-    def _run(self, tx_func_access_mode=None,
+    def _start_servers_and_driver(self, script, routing, db, impersonation):
+        if routing:
+            router_script = "router%s%%s.script"
+            if db:
+                router_script = router_script % ("_" + db)
+            else:
+                router_script = router_script % "_default_db"
+            if impersonation:
+                router_script = router_script % "_impersonation"
+            else:
+                router_script = router_script % ""
+            print("router script:", router_script)
+            self._router.start(path=self.script_path(router_script),
+                               vars={"#HOST#": self._router.host})
+        if routing and not db:
+            script += "_homedb.script"
+        else:
+            script += ".script"
+        print("server script:", script)
+        self._server.start(path=self.script_path(script))
+        if routing:
+            uri = "neo4j://%s" % self._router.address
+        else:
+            uri = "bolt://%s" % self._server.address
+        self._driver = Driver(self._backend, uri,
+                              types.AuthorizationToken("basic", principal="",
+                                                       credentials=""))
+
+    def _run(self, script, routing, tx_func_access_mode=None,
              session_args=None, session_kwargs=None,
              tx_args=None, tx_kwargs=None,
              run_args=None, run_kwargs=None):
@@ -53,92 +78,116 @@ class TestTxBeginParameters(TestkitTestCase):
             run_args = ()
         if run_kwargs is None:
             run_kwargs = {}
-        # FIXME: params are not used and dotnet fails when using them
+        self._start_servers_and_driver(script, routing,
+                                       session_kwargs.get("database"),
+                                       session_kwargs.get("impersonatedUser"))
         session = self._driver.session(*session_args, **session_kwargs)
-        if tx_func_access_mode is None:
-            tx = session.beginTransaction(*tx_args, **tx_kwargs)
-            work(tx)
-            tx.commit()
-        elif tx_func_access_mode == "w":
-            session.writeTransaction(work, *tx_args, **tx_kwargs)
-        elif tx_func_access_mode == "r":
-            session.readTransaction(work, *tx_args, **tx_kwargs)
-        else:
-            raise ValueError(tx_func_access_mode)
-        session.close()
+        try:
+            if tx_func_access_mode is None:
+                tx = session.beginTransaction(*tx_args, **tx_kwargs)
+                work(tx)
+                tx.commit()
+            elif tx_func_access_mode == "w":
+                session.writeTransaction(work, *tx_args, **tx_kwargs)
+            elif tx_func_access_mode == "r":
+                session.readTransaction(work, *tx_args, **tx_kwargs)
+            else:
+                raise ValueError(tx_func_access_mode)
+            self._server.done()
+        finally:
+            self._server.reset()
+            self._server._dump()
+            self._router.reset()
+            session.close()
+            self._driver.close()
 
     def _start_server(self, script):
         self._server.start(path=self.script_path(script))
 
     def test_access_mode_read(self):
-        self._start_server("access_mode_read.script")
-        self._run(session_args=("r",))
-        self._server.done()
+        for routing in (True, False):
+            with self.subTest("routing" if routing else "direct"):
+                self._run("access_mode_read", routing, session_args=("r",))
+                self._server.done()
 
     def test_tx_func_access_mode_read(self):
-        for session_access_mode in ("r", "w"):
-            with self.subTest("session_mode_" + session_access_mode):
-                self._start_server("access_mode_read.script")
-                try:
-                    self._run(session_args=(session_access_mode[0],),
+        for routing in (True, False):
+            for session_access_mode in ("r", "w"):
+                with self.subTest("session_mode_" + session_access_mode
+                                  + "_routing" if routing else "_direct"):
+                    self._run("access_mode_read", routing,
+                              session_args=(session_access_mode[0],),
                               tx_func_access_mode="r")
-                    self._server.done()
-                finally:
-                    self._server.reset()
 
     def test_access_mode_write(self):
-        self._start_server("access_mode_write.script")
-        self._run(session_args=("w",))
-        self._server.done()
+        for routing in (True, False):
+            with self.subTest("routing" if routing else "direct"):
+                self._run("access_mode_write", routing,
+                          session_args=("w",))
+                self._server.done()
 
     def test_tx_func_access_mode(self):
-        for session_access_mode in ("r", "w"):
-            with self.subTest("session_mode_" + session_access_mode):
-                self._start_server("access_mode_write.script")
-                try:
-                    self._run(session_args=(session_access_mode[0],),
+        for routing in (True, False):
+            for session_access_mode in ("r", "w"):
+                with self.subTest("session_mode_" + session_access_mode):
+                    self._run("access_mode_write", routing,
+                              session_args=(session_access_mode[0],),
                               tx_func_access_mode="w")
-                    self._server.done()
-                finally:
-                    self._server.reset()
 
     def test_parameters(self):
-        self._start_server("parameters.script")
-        self._run(session_args=("w",),
-                  run_kwargs={"params": {"p": types.CypherInt(1)}})
-        self._driver.close()
-        self._server.done()
+        for routing in (True, False):
+            with self.subTest("routing" if routing else "direct"):
+                self._run("parameters", routing,
+                          session_args=("w",),
+                          run_kwargs={"params": {"p": types.CypherInt(1)}})
 
     def test_bookmarks(self):
-        self._start_server("bookmarks.script")
-        self._run(session_args=("w",),
-                  session_kwargs={"bookmarks": ["b1", "b2"]})
-        self._server.done()
+        for routing in (True, False):
+            with self.subTest("routing" if routing else "direct"):
+                self._run("bookmarks", routing,
+                          session_args=("w",),
+                          session_kwargs={"bookmarks": ["b1", "b2"]})
 
     def test_tx_meta(self):
-        self._start_server("tx_meta.script")
-        self._run(session_args=("w",), tx_kwargs={"txMeta": {"akey": "aval"}})
-        self._server.done()
+        for routing in (True, False):
+            with self.subTest("routing" if routing else "direct"):
+                self._run("tx_meta", routing,
+                          session_args=("w",),
+                          tx_kwargs={"txMeta": {"akey": "aval"}})
 
     def test_timeout(self):
-        self._start_server("timeout.script")
-        self._run(session_args=("w",), tx_kwargs={"timeout": 17})
-        self._server.done()
+        for routing in (True, False):
+            with self.subTest("routing" if routing else "direct"):
+                self._run("timeout", routing,
+                          session_args=("w",), tx_kwargs={"timeout": 17})
+
+    def test_database(self):
+        for routing in (True, False)[1:]:
+            with self.subTest("routing" if routing else "direct"):
+                self._run("adb", routing,
+                          session_args=("w",),
+                          session_kwargs={"database": "adb"})
 
     @driver_feature(types.Feature.IMPERSONATION)
     def test_impersonation(self):
-        self._start_server("imp_user.script")
-        self._run(session_args=("w",),
-                  session_kwargs={"impersonatedUser": "that-other-dude"})
-        self._driver.close()
-        self._server.done()
+        for routing in (True, False):
+            with self.subTest("routing" if routing else "direct"):
+                self._run("imp_user", routing,
+                          session_args=("w",),
+                          session_kwargs={
+                              "impersonatedUser": "that-other-dude"
+                          })
 
     @driver_feature(types.Feature.IMPERSONATION)
     def test_combined(self):
-        self._start_server("combined.script")
-        self._run(session_args=("r",),
-                  run_kwargs={"params": {"p": types.CypherInt(1)}},
-                  session_kwargs={"bookmarks": ["b0"]},
-                  tx_kwargs={"txMeta": {"k": "v"}, "timeout": 11})
-        self._server.done()
-
+        for routing in (True, False):
+            with self.subTest("routing" if routing else "direct"):
+                self._run("combined", routing,
+                          session_args=("r",),
+                          run_kwargs={"params": {"p": types.CypherInt(1)}},
+                          session_kwargs={
+                              "bookmarks": ["b0"],
+                              "database": "adb",
+                              "impersonatedUser": "that-other-dude"
+                          },
+                          tx_kwargs={"txMeta": {"k": "v"}, "timeout": 11})
