1839958401b15d57c60e959fc4ae6d67cea90e7a
Robsdedude <rouven.bauer@neotechnology.com>
2021-11-02 16:36:02 +0100

    Add feature flags for different bolt versions (#286)
    Introduced feature flags:
     - `BOLT_3_0 = "Feature:Bolt:3.0"`
     - `BOLT_4_0 = "Feature:Bolt:4.0"`
     - `BOLT_4_1 = "Feature:Bolt:4.1"`
     - `BOLT_4_2 = "Feature:Bolt:4.2"`
     - `BOLT_4_3 = "Feature:Bolt:4.3"`
     - `BOLT_4_4 = "Feature:Bolt:4.4"`

--- tests/stub/summary/test_summary.py
+++ tests/stub/summary/test_summary.py
@@ -17,6 +17,8 @@ class TestSummary(TestkitTestCase):
 
     required_features = types.Feature.BOLT_4_4,
 
+    required_features = types.Feature.BOLT_4_4,
+
     def setUp(self):
         super().setUp()
         self._server = StubServer(9000)
