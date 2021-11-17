987f63c66753b294c1d129575e1fcfd048bd7385
Rouven Bauer <rouven.bauer@neo4j.com>
2021-11-17 15:45:39 +0100

    Move auth failure tests to one place

--- tests/neo4j/test_direct_driver.py
+++ tests/neo4j/test_direct_driver.py
@@ -1,5 +1,4 @@
 from nutkit import protocol as types
-from nutkit.frontend import Driver
 
 from ..shared import (
     dns_resolve_single,
@@ -9,7 +8,6 @@ from ..shared import (
 )
 from .shared import (
     cluster_unsafe_test,
-    get_authorization,
     get_driver,
     get_neo4j_host_and_http_port,
     get_neo4j_host_and_port,
@@ -76,23 +74,6 @@ class TestDirectDriver(TestkitTestCase):
         if get_driver_name() in ["python"]:
             self.assertEqual(e.exception.errorType,
                              "<class 'neo4j.exceptions.ServiceUnavailable'>")
-
-    def test_should_fail_on_incorrect_password(self):
-        def return_1(tx):
-            tx.run("RETURN 1").consume()
-
-        uri = "%s://%s:%d" % (get_neo4j_scheme(), *get_neo4j_host_and_port())
-        auth = get_authorization()
-        auth.credentials = auth.credentials + "-but-wrong!"
-        self._driver = Driver(self._backend, uri, auth)
-        self._session = self._driver.session("w")
-
-        with self.assertRaises(types.DriverError) as e:
-            self._session.read_transaction(return_1)
-
-        if get_driver_name() in ["python"]:
-            self.assertEqual(e.exception.errorType,
-                             "<class 'neo4j.exceptions.AuthError'>")
 
     @driver_feature(types.Feature.TMP_FULL_SUMMARY)
     def test_supports_multi_db(self):
