47392384e3c9b1f45ca397f3bd9b19c2705e1bd1
Rouven Bauer <rouven.bauer@neo4j.com>
2021-12-13 13:22:25 +0100

    Use TeamCity's REST API for downloading artifacts
    This has the main advantage that TestKit does not break every time the nightly
    Neo4j builds move to a new patch version.

--- teamcity/download.py
+++ teamcity/download.py
@@ -1,31 +1,86 @@
+import json
 from os import getenv
 from urllib import request
 
+_ROOT = "https://live.neo4j-build.io"
+_API_ROOT = f"{_ROOT}/app/rest"
+_BUILD_LOCATOR = "buildType:DriversTestkitNeo4jDockers"
 
-def download_artifact(build_id, build_spec, path):
+
+class _JsonHandler(request.BaseHandler):
+    def http_request(self, req):
+        if not req.has_header("Accept"):
+            req.add_header("Accept", "application/json")
+        return req
+
+    @staticmethod
+    def _res_as_json(res):
+        def read():
+            if (
+                "Content-Type" not in res.headers
+                or res.headers["Content-Type"].lower() != "application/json"
+            ):
+                raise ValueError("Response is not json")
+            encoding = res.info().get_content_charset("utf-8")
+            data = res.read()
+            return json.loads(data.decode(encoding))
+        return read
+
+    def http_response(self, req, res):
+        res.read_json = self._res_as_json(res)
+        return res
+
+    https_request = http_request
+    https_response = http_response
+
+
+def _get_opener(json_=False):
+    handlers = []
+
+    if json_:
+        handlers.append(_JsonHandler())
+
+    password_manager = request.HTTPPasswordMgrWithDefaultRealm()
+    password_manager.add_password(
+        None, _ROOT, getenv("TEAMCITY_USER"), getenv("TEAMCITY_PASSWORD")
+    )
+    handlers.append(request.HTTPBasicAuthHandler(password_manager))
+    return request.build_opener(*handlers)
+
+
+def _list_artifacts(filters=None):
+    if filters is None:
+        filters = ()
+    path = \
+        f"{_API_ROOT}/builds/{_BUILD_LOCATOR}/artifacts/children/neo4j-docker"
+    artifacts = _get_opener(json_=True).open(path).read_json()
+    return [
+        artifact["content"]["href"]
+        for artifact in artifacts["file"]
+        if all(filter_ in artifact["name"] for filter_ in filters)
+    ]
+
+
+def _download_artifact(path):
     """
     Download a neo4j artifact from TeamCity.
 
     @returns: http response
     """
-    root = "https://live.neo4j-build.io"
-    path = "{}/repository/download/{}/{}/{}".format(root, build_id, build_spec,
-                                                    path)
-    user = getenv("TEAMCITY_USER")
-    pasw = getenv("TEAMCITY_PASSWORD")
-    password_mgr = request.HTTPPasswordMgrWithDefaultRealm()
-    password_mgr.add_password(None, root, user, pasw)
-    handler = request.HTTPBasicAuthHandler(password_mgr)
-    opener = request.build_opener(handler)
-    return opener.open(path)
+    path = _ROOT + path
+    return _get_opener().open(path)
 
 
 class DockerImage:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, *filters):
+        self.filters = filters
 
     def get(self):
-        return download_artifact(
-            "DriversTestkitNeo4jDockers", ".lastSuccessful",
-            "neo4j-docker/" + self.name
-        )
+        artifacts = _list_artifacts(filters=self.filters)
+        if not len(artifacts) == 1:
+            raise ValueError(
+                "Filter matched not exactly one image.\n"
+                f"Filters: {self.filters}\n"
+                f"Matches: {artifacts}"
+            )
+        return _download_artifact(artifacts[0])
