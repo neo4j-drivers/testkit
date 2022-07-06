3b51c32e45621d5c19e74d088a8e072eedecfffe
Rouven Bauer <rouven.bauer@neo4j.com>
2022-07-06 11:54:15 +0200

    Add feature flag for result status after broken record

--- tests/stub/types/test_temporal_types.py
+++ tests/stub/types/test_temporal_types.py
@@ -90,19 +90,32 @@ class _TestTemporalTypes(TestkitTestCase):
         self.assertEqual(tx_count, 1)
 
     def _test_unknown_then_known_zoned_date_time(self, patched):
+        result_is_valid_after_unknown_zone = self.driver_supports_features(
+            types.Feature.DETAIL_RESULT_STREAM_WORKS_AFTER_BROKEN_RECORD
+        )
+
         def work(tx):
             res = tx.run("MATCH (n:State) RETURN n.founded AS dt")
             with self.assertRaises(types.DriverError) as exc:
                 res.next()
             self.assertIn("Europe/Neo4j", exc.exception.msg)
 
-            rec = res.next()
-            self.assertIsInstance(rec, types.Record)
-            self.assertEqual(len(rec.values), 1)
-            self.assertEqual(
-                rec.values[0],
-                types.CypherDateTime(1970, 1, 1, 1, 0, 0, 0, utc_offset_s=1800)
-            )
+            if result_is_valid_after_unknown_zone:
+                rec = res.next()
+                self.assertIsInstance(rec, types.Record)
+                self.assertEqual(len(rec.values), 1)
+                self.assertEqual(
+                    rec.values[0],
+                    types.CypherDateTime(1970, 1, 1, 1, 0, 0, 0,
+                                         utc_offset_s=1800)
+                )
+            else:
+                with self.assertRaises(types.DriverError):
+                    rec = res.next()
+                    if isinstance(rec, types.NullRecord):
+                        # that's fine, too
+                        raise types.DriverError("Result is broken")
+                raise exc.exception  # abort the transaction
 
         script = "echo_unknown_then_known_zoned_date_time.script"
         if patched:
@@ -110,7 +123,16 @@ class _TestTemporalTypes(TestkitTestCase):
         self._start_server(script)
         self._create_direct_driver()
         self._session = self._driver.session("w")
-        self._session.read_transaction(work)
+        if result_is_valid_after_unknown_zone:
+            self._session.read_transaction(work)
+            self._server.done()
+            self.assertEqual(self._server.count_requests("COMMIT"), 1)
+            self.assertEqual(self._server.count_requests("ROLLBACK"), 0)
+        else:
+            with self.assertRaises(types.DriverError):
+                self._session.read_transaction(work)
+            self._server.done()
+            self.assertEqual(self._server.count_requests("COMMIT"), 0)
 
 
 class TestTemporalTypesV3x0(_TestTemporalTypes):
