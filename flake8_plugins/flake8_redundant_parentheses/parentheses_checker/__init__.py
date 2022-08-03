d5cbd31efb66253ee823a62f91a6c4f032b8d8f0
IvanPrychantovskyi <109072205+IvanPrychantovskyi@users.noreply.github.com>
2022-08-03 17:22:44 +0100

    parentheses checker

--- flake8_plugins/flake8_redundant_parentheses/parentheses_checker/__init__.py
+++ flake8_plugins/flake8_redundant_parentheses/parentheses_checker/__init__.py
@@ -1,34 +1,27 @@
 import ast
 import importlib.metadata
-from typing import (
-    Any,
-    Generator,
-    List,
-    Tuple,
-    Type,
-)
+from typing import Any
+from typing import Generator
+from typing import List
+from typing import Tuple
+from typing import Type
 
 
 class Visitor(ast.NodeVisitor):
-    def __init__(self, tree, read_lines, parentheses_dict):
+    def __init__(self, vals, tree):
         self.problems: List[Tuple[int, int, str]] = []
+        self.vals = vals
         self.tree = tree
-        self.read_lines = read_lines
-        self.parentheses = parentheses_dict
-
-    def visit_Something(self, node, msg) -> None:
-        vals = []
-        for i in self.parentheses.values():
-            vals.append(check_trees(self.read_lines, self.tree, i))
-
-        if True in vals:
-            self.problems.append((node.lineno, node.col_offset, msg))
-
-        self.generic_visit(node)
 
     def visit_Module(self, node: ast.Module) -> None:
         msg = 'PAR001: Too many parentheses'
-        for node_ in node.body:
+        ex = []
+        for node_ in ast.walk(self.tree):
+            if isinstance(node_, ast.BinOp) or isinstance(node_, ast.BoolOp):
+                for node_op in ast.iter_child_nodes(node_):
+                    if isinstance(node_op, ast.BinOp) or isinstance(node_op, ast.BoolOp):
+                        ex.append([node_op.col_offset - 1, node_op.end_col_offset])
+
             if isinstance(node_, ast.Assign):
                 for targ in node_.targets:
                     if isinstance(targ, ast.Tuple):
@@ -36,21 +29,53 @@ class Visitor(ast.NodeVisitor):
                                               'PAR002: Dont use parentheses for unpacking'))
                         continue
 
-            self.visit_Something(node_, msg)
+            for node_tup in ast.iter_child_nodes(node_):
+                if isinstance(node_tup, ast.Tuple):
+                    for node__ in node_tup.elts:
+                        if node_tup.end_col_offset - node__.end_col_offset != 2:
+                            self.problems.append((node_.lineno, node_.col_offset,
+                                                 'PAR003: Use parentheses for tuple literal'))
+                            continue
+                        if node_tup.end_col_offset - node_.end_col_offset == 0:
+                            ex.append([node_tup.col_offset, node_tup.end_col_offset - 1])
+                            continue
+
+        for node_ in node.body:
+            if ex:
+                for j in ex:
+                    for i in self.vals:
+                        if i is not False and i != j:
+                            self.problems.append((node_.lineno, node_.col_offset, msg))
+                        continue
+
+            elif not self.vals:
+                break
+
+            elif not self.problems:
+                for j in self.vals:
+                    if j is False:
+                        continue
+                    else:
+                        self.problems.append((node_.lineno, node_.col_offset, msg))
+                        break
+        self.generic_visit(node)
 
 
 class Plugin:
     name = __name__
-    version = importlib.metadata.version("flake8_redundant_parentheses")
+    version = importlib.metadata.version(__name__)
 
     def __init__(self, tree: ast.AST, read_lines, file_tokens):
         self._tree = tree
-        self.file_tokens = list(file_tokens)
-        self._lines_list = "".join(read_lines())
-        self.parentheses_dict = check(self.file_tokens)
+        self.vals = []
+        self.dump_tree = ast.dump(tree)
+        self.dict = check(list(file_tokens))
+        self._lines_list = "".join(read_lines)
+        for i in self.dict.values():
+            self.vals.append(check_trees(self._lines_list, self.dump_tree, i))
 
     def run(self) -> Generator[Tuple[int, int, str, Type[Any]], None, None]:
-        visitor = Visitor(self._tree, self._lines_list, self.parentheses_dict)
+        visitor = Visitor(self.vals, self._tree)
         visitor.visit(self._tree)
         for line, col, MSG in visitor.problems:
             yield line, col, MSG, type(self)
@@ -92,7 +117,9 @@ def check_trees(list_, start_tree, u):
     list__ = "\n".join(list__)
     try:
         tree = ast.parse(list__)
-        if ast.dump(tree) == ast.dump(start_tree):
-            return True
+        if ast.dump(tree) == start_tree:
+            return [u[0][1], u[1][1]]
+        else:
+            return False
     except:
         return False
