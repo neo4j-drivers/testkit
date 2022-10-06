neo4j-drivers/testkit

commit 1917fdd82da69f9bfdebe48ee41a2cc5d1e63e57

Ignore non-0 exit code from killed processes in docker. (#527)
`docker stop` (or `docker rm -f`) will first send `SIGTERM` to the process
started with `docker run` (`docker exec` processes don't receive a signal at
all). When not shutting down gracefully, they'll be `SIGKILL`ed. Either way,
the exit code of the `docker run` command will be (assuming running on Linux)
non-zero https://tldp.org/LDP/abs/html/exitcodes.html. Therefore, we want to
ignore any failures (non-zero exits) during shut down of docker containers.

docker.py
@@ -19,36 +19,54 @@ def _docker_path(path):
 
 
 def _subprocess_run(cmd, *args, log_path=None, background=False, **kwargs):
-    def run():
-        if not log_path:
-            print(cmd)
-            subprocess.run(cmd, *args, stdout=None, stderr=None, **kwargs)
-        else:
-            out_path = os.path.join(log_path, "out.log")
-            err_path = os.path.join(log_path, "err.log")
-            with open(out_path, "a") as out_fd:
-                with open(err_path, "a") as err_fd:
-                    out_fd.write(str(cmd) + "\n")
-                    out_fd.flush()
-                    err_fd.write(str(cmd) + "\n")
-                    err_fd.flush()
-                    print(cmd)
-                    subprocess.run(cmd, *args,
-                                   stdout=out_fd, stderr=err_fd, **kwargs)
-                    out_fd.write("\n")
-                    out_fd.flush()
-                    err_fd.write("\n")
-                    err_fd.flush()
+    class Runner:
+        def __init__(self):
+            self.stopping = False
 
+        def _process_run(self):
+            try:
+                subprocess.run(cmd, *args, stdout=None, stderr=None, **kwargs)
+            except subprocess.CalledProcessError:
+                # ignore when shutting down anyway
+                if not self.stopping:
+                    raise
+
+        def run(self):
+            if not log_path:
+                print(cmd)
+                self._process_run()
+            else:
+                out_path = os.path.join(log_path, "out.log")
+                err_path = os.path.join(log_path, "err.log")
+                with open(out_path, "a") as out_fd:
+                    with open(err_path, "a") as err_fd:
+                        out_fd.write(str(cmd) + "\n")
+                        out_fd.flush()
+                        err_fd.write(str(cmd) + "\n")
+                        err_fd.flush()
+                        print(cmd)
+                        try:
+                            self._process_run()
+                        finally:
+                            out_fd.write("\n")
+                            out_fd.flush()
+                            err_fd.write("\n")
+                            err_fd.flush()
+
+    runner = Runner()
     if not background:
-        run()
+        runner.run()
     else:
-        Thread(target=run, daemon=True).start()
+        Thread(target=runner.run, daemon=True).start()
+    return runner
 
 
 class Container:
-    def __init__(self, name):
+    def __init__(self, name, runners=None):
         self.name = name
+        if runners is None:
+            runners = []
+        self.runners = runners
 
     def _add(self, cmd, workdir, env_map):
         if workdir:
@@ -63,8 +81,11 @@ class Container:
         self._add(cmd, workdir, env_map)
         cmd.append(self.name)
         cmd.extend(command)
-        _subprocess_run(cmd, log_path=log_path, background=background,
-                        check=True)
+        self.runners.append(
+            _subprocess_run(
+                cmd, log_path=log_path, background=background, check=True
+            )
+        )
 
     def exec(self, command, workdir=None, env_map=None, log_path=None):
         self._exec(command, workdir=workdir, env_map=env_map,
@@ -76,6 +97,8 @@ class Container:
                    log_path=log_path, background=True)
 
     def rm(self):
+        for runner in self.runners:
+            runner.stopping = True
         cmd = ["docker", "rm", "-f", "-v", self.name]
         print(cmd)
         subprocess.run(cmd, check=False,
@@ -86,8 +109,10 @@ class Container:
 def create_or_replace(image, name, command=None, mount_map=None, host_map=None,
                       port_map=None, env_map=None, working_folder=None,
                       network=None, aliases=None):
-    print(["docker", "rm", "-fv", name])
-    subprocess.run(["docker", "rm", "-fv", name], check=True)
+    if name in _running:
+        _running[name].rm()
+    else:
+        subprocess.run(["docker", "rm", "-fv", name], check=True)
     cmd = ["docker", "create", "--name", name]
     if mount_map is not None:
         for k in mount_map:
@@ -121,8 +146,8 @@ def create_or_replace(image, name, command=None, mount_map=None, host_map=None,
 def start(name):
     cmd = ["docker", "start", name]
     print(cmd)
-    subprocess.run(cmd, check=True)
-    container = Container(name)
+    runner = _subprocess_run(cmd, check=True)
+    container = Container(name, runners=[runner])
     _running[name] = container
     return container
 
@@ -162,8 +187,9 @@ def run(image, name, command=None, mount_map=None, host_map=None,
     cmd.append(image)
     if command:
         cmd.extend(command)
-    _subprocess_run(cmd, check=True, log_path=log_path, background=background)
-    container = Container(name)
+    runner = _subprocess_run(cmd, check=True, log_path=log_path,
+                             background=background)
+    container = Container(name, runners=[runner])
     _running[name] = container
     return container
 

commit 16d1e0e2228630fbda7b000825d6247015cfa21b

Fix docker run commands not logging output
Broke in https://github.com/neo4j-drivers/testkit/pull/527

docker.py
@@ -21,11 +21,14 @@ def _docker_path(path):
 def _subprocess_run(cmd, *args, log_path=None, background=False, **kwargs):
     class Runner:
         def __init__(self):
+            if log_path and "stdout" in kwargs or "stderr" in kwargs:
+                raise ValueError("Cannot specify log_path with "
+                                 "stdout or stderr")
             self.stopping = False
 
         def _process_run(self):
             try:
-                subprocess.run(cmd, *args, stdout=None, stderr=None, **kwargs)
+                subprocess.run(cmd, *args, **kwargs)
             except subprocess.CalledProcessError:
                 # ignore when shutting down anyway
                 if not self.stopping:
@@ -40,6 +43,7 @@ def _subprocess_run(cmd, *args, log_path=None, background=False, **kwargs):
                 err_path = os.path.join(log_path, "err.log")
                 with open(out_path, "a") as out_fd:
                     with open(err_path, "a") as err_fd:
+                        kwargs.update(stdout=out_fd, stderr=err_fd)
                         out_fd.write(str(cmd) + "\n")
                         out_fd.flush()
                         err_fd.write(str(cmd) + "\n")