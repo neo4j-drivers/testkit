8cddfa258492654f80ba8240c295f36d2bbb5cdc
grant lodge <6323995+thelonelyvulpes@users.noreply.github.com>
2022-10-24 17:12:31 +0100

    full suite for driver config, undo validation for mapping

--- tests/stub/driver_parameters/test_notification_filters.py
+++ tests/stub/driver_parameters/test_notification_filters.py
@@ -9,12 +9,15 @@ from tests.stub.shared import StubServer
 class TestNotificationFilters(TestkitTestCase):
     required_features = (types.Feature.BOLT_5_1,
                          types.Feature.NOTIFICATION_FILTERS)
-    script_name = "summary_with_notifications.script"
+
     map_data = None
+    _auth = types.AuthorizationToken("basic", principal="neo4j",
+                                    credentials="pass")
 
     def setUp(self):
         super().setUp()
         self._server = StubServer(9010)
+        self._uri = "bolt://%s" % self._server.address
         self._driver = None
 
     def tearDown(self):
@@ -25,14 +28,12 @@ class TestNotificationFilters(TestkitTestCase):
         return super().tearDown()
 
     def _new_driver(self, notifications):
-        uri = "bolt://%s" % self._server.address
-        auth = types.AuthorizationToken("basic", principal="neo4j",
-                                        credentials="pass")
-        return Driver(self._backend, uri, auth,
+        return Driver(self._backend, self._uri, self._auth,
                       notification_filters=notifications)
 
     def _run_test_get_summary(self, filters, script_params):
-        self._server.start(self.script_path(self.script_name),
+        script_name = "notification_filters.script"
+        self._server.start(self.script_path(script_name),
                            vars_=script_params)
         self._driver = self._new_driver(filters)
         session = self._driver.session("w", database="neo4j")
@@ -51,71 +52,98 @@ class TestNotificationFilters(TestkitTestCase):
             "#FILTERS#": cfg["bolt_filters"],
             "#EMIT#": notifications
         }
-        summary = self._run_test_get_summary(cfg["tk_filters"], script_params)
+        self._run_test_get_summary(cfg["tk_filters"], script_params)
         self._server.done()
 
-        # todo consider removing from tests, we aren't testing mapping
-        #  the server shutting down successfully is proof we configured.
-        expected_notifications = [{
-            "severity": cfg["in_sev"],
-            "severityLevel": cfg["enum_sev"],
-            "rawCategory": cfg["in_cat"],
-            "category": cfg["enum_cat"],
-            "description": "ignore me",
-            "code": "MadeUp",
-            "title": "nonsense."
-        }]
-        self.assertListEqual(expected_notifications, summary.notifications)
+    def test_no_filter_notifications(self):
+        cfg = {
+            "tk_filters": ["None"],
+            "in_sev": "WARNING",
+            "in_cat": "DEPRECATION",
+            "bolt_filters": '[]',
+        }
+        self._test_single_notification_filter(cfg)
 
-    def test_all_filter_notifications(self):
+    def test_default_server_filters(self):
+        no_filter_script = "default_notification_filters.script"
+        self._server.start(self.script_path(no_filter_script))
+        self._driver = Driver(self._backend, self._uri, self._auth)
+
+        session = self._driver.session("w", database="neo4j")
+        cursor = session.run("CREATE (:node)")
+        cursor.consume()
+        self._server.done()
+
+    def test_filter_notifications(self):
         cfgs = [{
             "name": "all",
             "tk_filters": ["ALL.ALL"],
             "in_sev": "WARNING",
-            "enum_sev": "WARNING",
             "in_cat": "DEPRECATION",
-            "enum_cat": "DEPRECATION",
             "bolt_filters": '["*.*"]',
         }, {
             "name": "all.query",
             "tk_filters": ["ALL.QUERY"],
             "in_sev": "WARNING",
             "in_cat": "QUERY",
-            "enum_sev": "WARNING",
-            "enum_cat": "QUERY",
             "bolt_filters": '["*.QUERY"]',
-        }]
-        for cfg in cfgs:
-            with self.subTest(name=cfg["name"]):
-                self._test_single_notification_filter(cfg)
-
-    def test_warning_filter_notifications(self):
-        cfgs = [{
+        }, {
             "name": "warning.all",
             "tk_filters": ["WARNING.ALL"],
             "in_sev": "WARNING",
             "in_cat": "DEPRECATION",
-            "enum_sev": "WARNING",
-            "enum_cat": "DEPRECATION",
             "bolt_filters": '["WARNING.*"]',
-        }]
-
-        # add cases
-        for cfg in cfgs:
-            with self.subTest(name=cfg["name"]):
-                self._test_single_notification_filter(cfg)
-
-    def test_info_filter_notifications(self):
-        cfgs = [{
+        }, {
+            "name": "warning.deprecation",
+            "tk_filters": ["WARNING.DEPRECATION"],
+            "in_sev": "WARNING",
+            "in_cat": "DEPRECATION",
+            "bolt_filters": '["WARNING.DEPRECATION"]',
+        }, {
+            "name": "warning.hint",
+            "tk_filters": ["WARNING.HINT"],
+            "in_sev": "WARNING",
+            "in_cat": "HINT",
+            "bolt_filters": '["WARNING.HINT"]',
+        }, {
+            "name": "warning.query",
+            "tk_filters": ["WARNING.QUERY"],
+            "in_sev": "WARNING",
+            "in_cat": "QUERY",
+            "bolt_filters": '["WARNING.QUERY"]',
+        }, {
+            "name": "warning.unsupported",
+            "tk_filters": ["WARNING.UNSUPPORTED"],
+            "in_sev": "WARNING",
+            "in_cat": "UNSUPPORTED",
+            "bolt_filters": '["WARNING.UNSUPPORTED"]',
+        },{
             "name": "info.all",
             "tk_filters": ["INFORMATION.ALL"],
             "in_sev": "INFORMATION",
             "in_cat": "DEPRECATION",
-            "enum_sev": "INFORMATION",
-            "enum_cat": "DEPRECATION",
             "bolt_filters": '["INFORMATION.*"]',
+        },{
+            "name": "info.runtime",
+            "tk_filters": ["INFORMATION.RUNTIME"],
+            "in_sev": "INFORMATION",
+            "in_cat": "RUNTIME",
+            "bolt_filters": '["INFORMATION.RUNTIME"]',
+        },{
+            "name": "info.query",
+            "tk_filters": ["INFORMATION.QUERY"],
+            "in_sev": "INFORMATION",
+            "in_cat": "QUERY",
+            "bolt_filters": '["INFORMATION.QUERY"]',
+        },{
+            "name": "info.performance",
+            "tk_filters": ["INFORMATION.PERFORMANCE"],
+            "in_sev": "INFORMATION",
+            "in_cat": "PERFORMANCE",
+            "bolt_filters": '["INFORMATION.PERFORMANCE"]',
         }]
-        # add cases
         for cfg in cfgs:
             with self.subTest(name=cfg["name"]):
                 self._test_single_notification_filter(cfg)
+
+
