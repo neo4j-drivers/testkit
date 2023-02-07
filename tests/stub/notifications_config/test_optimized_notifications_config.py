2ca41c95e433c12812a90c588a9d915af63a7808
grant lodge <6323995+thelonelyvulpes@users.noreply.github.com>
2023-02-07 17:09:37 +0000

    remove redundant test, as it is defined in the base configs

--- tests/stub/notifications_config/test_optimized_notifications_config.py
+++ tests/stub/notifications_config/test_optimized_notifications_config.py
@@ -37,11 +37,6 @@ class TestOptimizedNotificationsConfig(NotificationsBase):
                 self._run_test(config["protocol"], config["script"])
             self._server.reset()
 
-    def test_omit_disabled_categories_when_notifications_disabled(self):
-        self._run_test({"min_sev": "OFF", "dis_cats": ["DEPRECATION"]},
-                       {"#NOTIS#": '"noti_min_sev":"OFF", '})
-        self._server.reset()
-
     def test_omit_disabled_categories_when_empty(self):
         self._run_test({"min_sev": "INFORMATION", "dis_cats": []},
                        {"#NOTIS#": '"noti_min_sev":"INFORMATION", '})
