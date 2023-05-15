1946f5879f5e582ff1c6f018d030d2d00f5a530b
Robsdedude <rouven.bauer@neo4j.com>
2023-05-15 15:39:03 +0200

    Adopt tests for structured bolt agent
    As the ADR got updated the tests needed adjusting:
     * `user_agent` remains untouched
     * `bolt_agent` becomes a dictionary instead of a string
    
    Also added empty user_agent as test case.

--- tests/stub/driver_parameters/test_client_agent_strings.py
+++ tests/stub/driver_parameters/test_client_agent_strings.py
@@ -1,10 +1,8 @@
 import abc
 from contextlib import contextmanager
-import json
-from typing import Optional
 
-from nutkit.frontend import Driver
 import nutkit.protocol as types
+from nutkit.frontend import Driver
 from tests.shared import TestkitTestCase
 from tests.stub.shared import StubServer
 
@@ -14,10 +12,6 @@ class _ClientAgentStringsTestBase(TestkitTestCase, abc.ABC):
     @property
     @abc.abstractmethod
     def version_folder(self) -> str:
-        ...
-
-    @abc.abstractmethod
-    def get_default_agent(self) -> Optional[str]:
         ...
 
     def setUp(self):
@@ -72,28 +66,17 @@ class _ClientAgentStringsTestBase(TestkitTestCase, abc.ABC):
             result = session.run("RETURN 1 AS n")
             list(result)
 
-    def _extract_bolt_agent(self) -> str:
-        self._start_server("user_agent_default.script",
-                           version_folder="v5x3")
-        self._session_run_return_1()
-        self._server.reset()
-        hellos = self._server.get_requests("HELLO ")
-        assert len(hellos) == 1
-        hello = hellos[0]
-        hello_extra = hello[len("HELLO "):].strip()
-        hello_extra = json.loads(hello_extra)
-        return hello_extra["{}"]["bolt_agent"]
-
     def _test_default_user_agent(self):
-        vars_ = {
-            "#USER_AGENT#": self.get_default_agent(),
-        }
-        self._start_server("user_agent_default.script", vars_=vars_)
+        self._start_server("user_agent_default.script")
         self._session_run_return_1()
 
     def _test_custom_user_agent(self):
         self._start_server("user_agent_custom.script")
         self._session_run_return_1(user_agent="Hello, I'm a banana 🍌!")
+
+    def _test_empty_user_agent(self):
+        self._start_server("user_agent_empty.script")
+        self._session_run_return_1(user_agent="")
 
 
 class TestClientAgentStringsV5x2(_ClientAgentStringsTestBase):
@@ -101,16 +84,14 @@ class TestClientAgentStringsV5x2(_ClientAgentStringsTestBase):
     version_folder = "v5x2"
     required_features = (types.Feature.BOLT_5_2,)
 
-    def get_default_agent(self) -> Optional[str]:
-        if self.driver_supports_features(types.Feature.BOLT_5_3):
-            return json.dumps(self._extract_bolt_agent())
-        return '{"U": "*"}'
-
     def test_default_user_agent(self):
         super()._test_default_user_agent()
 
     def test_custom_user_agent(self):
         super()._test_custom_user_agent()
+
+    def test_empty_user_agent(self):
+        super()._test_empty_user_agent()
 
 
 class TestClientAgentStringsV5x3(_ClientAgentStringsTestBase):
@@ -118,19 +99,11 @@ class TestClientAgentStringsV5x3(_ClientAgentStringsTestBase):
     version_folder = "v5x3"
     required_features = (types.Feature.BOLT_5_3,)
 
-    def get_default_agent(self) -> Optional[str]:
-        return None
-
     def test_default_user_agent(self):
         super()._test_default_user_agent()
 
     def test_custom_user_agent(self):
         super()._test_custom_user_agent()
 
-    def test_user_agent_is_bolt_agent(self):
-        bolt_agent = self._extract_bolt_agent()
-        vars_ = {
-            "#AGENT_STRING#": json.dumps(bolt_agent),
-        }
-        self._start_server("user_agent_is_bolt_agent.script", vars_=vars_)
-        self._session_run_return_1(user_agent=bolt_agent)
+    def test_empty_user_agent(self):
+        super()._test_empty_user_agent()
