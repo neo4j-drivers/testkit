c31bcd55551b9192b5720a9c6bae8d64fc1ab7c4
Robsdedude <rouven.bauer@neo4j.com>
2024-09-24 16:48:14 +0200

    WiP: add more GQL error tests

--- tests/stub/errors/test_errors.py
+++ tests/stub/errors/test_errors.py
@@ -4,6 +4,7 @@ from abc import (
     abstractmethod,
 )
 from contextlib import contextmanager
+from copy import deepcopy
 
 import nutkit.protocol as types
 from nutkit.frontend import Driver
@@ -69,7 +70,7 @@ class TestError5x5(_ErrorTestCase):
         types.Feature.BOLT_5_7,
     )
 
-    bolt_version = "5.6"
+    bolt_version = "5.7"
 
     def test_error(self):
         for (error_code, retryable) in (
@@ -88,7 +89,7 @@ class TestError5x5(_ErrorTestCase):
                 self.assertEqual(error.code, error_code)
                 self.assertEqual(error.msg, error_message)
                 self.assertEqual(error.retryable, retryable)
-                if self.driver_supports_features(types.Feature.BOLT_5_6):
+                if self.driver_supports_features(types.Feature.BOLT_5_7):
                     self.assertEqual(error.gql_status, "50N42")
                     expected_desc = (
                         "error: "
@@ -97,8 +98,8 @@ class TestError5x5(_ErrorTestCase):
                     )
                     self.assertEqual(error.status_description, expected_desc)
                     self.assertIsNone(error.cause)
-                    # TODO: TBD
-                    # self.assertEqual(error.classification, "UNKNOWN")
+                    self.assertEqual(error.classification, "UNKNOWN")
+                    self.assertEqual(error.raw_classification, None)
 
 
 DEFAULT_DIAG_REC = {
@@ -114,6 +115,172 @@ class TestError5x7(_ErrorTestCase):
     )
 
     bolt_version = "5.7"
+
+    # TODO: tests:
+    #  * [x] test classification parsing
+    #  * [x] test diagnostic record filling with default values
+    #  * [x] test diagnostic record with "future" values
+    #  * [x] all of the above in a cause as well
+    #  * [ ] test cause nesting
+    #  * [ ] test GQL values with older servers
+
+    def _make_test_error_data(
+        self,
+        status=...,
+        description=...,
+        message=...,
+        code=...,
+        diagnostic_record=...,
+        extra_diag_rec=None,
+        del_diag_rec=None,
+        cause=None,
+    ):
+        data = {}
+        if status is not None:
+            data["gql_status"] = "01N00" if status is ... else status
+        if description is not None:
+            data["description"] = (
+                "cool class - mediocre subclass"
+                if description is ...
+                else description
+            )
+        if message is not None:
+            data["message"] = (
+                "Sever ain't cool with this, John Doe!"
+                if message is ...
+                else message
+            )
+        if code is not None:
+            data["neo4j_code"] = (
+                "Neo.ClientError.User.Uncool" if code is ... else code
+            )
+        if diagnostic_record is ...:
+            data["diagnostic_record"] = {
+                "OPERATION": "",
+                "OPERATION_CODE": "0",
+                "CURRENT_SCHEMA": "/",
+                "_classification": "CLIENT_ERROR",
+                "_status_parameters": {"userName": "John Doe"},
+            }
+        elif diagnostic_record is not None:
+            data["diagnostic_record"] = diagnostic_record
+        if extra_diag_rec is not None:
+            data["diagnostic_record"].update(extra_diag_rec)
+        if del_diag_rec:
+            for key in del_diag_rec:
+                del data["diagnostic_record"][key]
+        if cause is not None:
+            data["cause"] = cause
+        return data
+
+    def _assert_is_test_error(self, exc, data):
+        self.assertEqual(exc.gql_status, data["gql_status"])
+        self.assertEqual(exc.status_description, data["description"])
+        self.assertEqual(exc.msg, data["message"])
+        if "neo4j_code" in data:
+            self.assertEqual(exc.code, data["neo4j_code"])
+        else:
+            self.assertFalse(hasattr(exc, "code"))
+        expected_diag_rec = deepcopy(data.get("diagnostic_record", {}))
+        for k, v in DEFAULT_DIAG_REC.items():
+            expected_diag_rec.setdefault(k, v)
+        self.assertEqual(
+            exc.diagnostic_record,
+            types.as_cypher_type(expected_diag_rec).value,
+        )
+        expected_raw_classification = expected_diag_rec.get("_classification")
+        if isinstance(expected_raw_classification, str):
+            self.assertEqual(
+                exc.raw_classification,
+                expected_raw_classification,
+            )
+        expected_classification = "UNKNOWN"
+        if expected_raw_classification in {
+            "CLIENT_ERROR",
+            "DATABASE_ERROR",
+            "TRANSIENT_ERROR",
+        }:
+            expected_classification = expected_raw_classification
+        self.assertEqual(exc.classification, expected_classification)
+        if "cause" in data:
+            self._assert_is_test_error(exc.cause, data["cause"])
+
+    def test_error_classification(self):
+        for as_cause in (False, True):
+            for classification in (
+                "CLIENT_ERROR",
+                "DATABASE_ERROR",
+                "TRANSIENT_ERROR",
+                "SECURITY_ERROR",  # made up classification
+            ):
+                with self.subTest(
+                    as_cause=as_cause,
+                    classification=classification,
+                ):
+                    error_data = self._make_test_error_data(
+                        extra_diag_rec={"_classification": classification},
+                        code=None if as_cause else ...,
+                    )
+                    if as_cause:
+                        error_data = self._make_test_error_data(
+                            cause=error_data,
+                        )
+                    error = self.get_error(error_data)
+                    self._assert_is_test_error(error, error_data)
+
+    def test_filling_default_diagnostic_record(self):
+        for as_cause in (False, True):
+            with self.subTest(as_cause=as_cause):
+                error_data = self._make_test_error_data(
+                    diagnostic_record=None,
+                    code=None if as_cause else ...,
+                )
+                if as_cause:
+                    error_data = self._make_test_error_data(cause=error_data)
+                error = self.get_error(error_data)
+                self._assert_is_test_error(error, error_data)
+
+    def test_filling_default_value_in_diagnostic_record(self):
+        for as_cause in (False, True):
+            for missing_key in (
+                "CURRENT_SCHEMA",
+                "OPERATION",
+                "OPERATION_CODE",
+            ):
+                with self.subTest(as_cause=as_cause, missing_key=missing_key):
+                    error_data = self._make_test_error_data(
+                        del_diag_rec=[missing_key],
+                        code=None if as_cause else ...,
+                    )
+                    if as_cause:
+                        error_data = self._make_test_error_data(
+                            cause=error_data
+                        )
+                    error = self.get_error(error_data)
+                    self._assert_is_test_error(error, error_data)
+
+    def test_keeps_rubbish_in_diagnostic_record(self):
+        for as_cause in (False, True):
+            with self.subTest(as_cause=as_cause):
+                diagnostic_record = {
+                    "foo": "bar",
+                    "_baz": 1.2,
+                    "OPERATION": None,
+                    "CURRENT_SCHEMA": {"uh": "oh!"},
+                    "OPERATION_CODE": False,
+                    "_classification": 42,
+                    # stub script will interpret this as JOLT bytes
+                    "_status_parameters": {"#": "00 FF"},
+                }
+                error_data = self._make_test_error_data(
+                    diagnostic_record=diagnostic_record,
+                    code=None if as_cause else ...,
+                )
+                if as_cause:
+                    error_data = self._make_test_error_data(cause=error_data)
+                error = self.get_error(error_data)
+                diagnostic_record["_status_parameters"] = b"\x00\xff"
+                self._assert_is_test_error(error, error_data)
 
     def test_error(self):
         error_status = "01N00"
