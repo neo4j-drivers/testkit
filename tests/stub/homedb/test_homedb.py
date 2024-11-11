2282be6296a547072822ea24d5757d4186301424
Robsdedude <rouven.bauer@neo4j.com>
2024-11-11 16:13:05 +0100

    Fix shared tests in base class being collected

--- tests/stub/homedb/test_homedb.py
+++ tests/stub/homedb/test_homedb.py
@@ -9,181 +9,197 @@ from tests.shared import (
 from tests.stub.shared import StubServer
 
 
-class _HomeDbWithoutCacheTestCase(abc.ABC, TestkitTestCase):
+class _Base:
+    # nested class to avoid test runner picking up the tests from the base
 
-    def setUp(self):
-        super().setUp()
-        self._router = StubServer(9000)
-        self._reader1 = StubServer(9010)
-        self._reader2 = StubServer(9011)
-        self._auth_token = types.AuthorizationToken("basic", principal="p",
-                                                    credentials="c")
-        self._uri = f"neo4j://{self._router.address}"
+    class HomeDbWithoutCache(abc.ABC, TestkitTestCase):
 
-    def tearDown(self):
-        self._reader1.reset()
-        self._reader2.reset()
-        self._router.reset()
-        super().tearDown()
+        def setUp(self):
+            super().setUp()
+            self._router = StubServer(9000)
+            self._reader1 = StubServer(9010)
+            self._reader2 = StubServer(9011)
+            self._auth_token = types.AuthorizationToken("basic", principal="p",
+                                                        credentials="c")
+            self._uri = f"neo4j://{self._router.address}"
 
-    def start_server(self, server, *path):
-        server.start(
-            path=self.script_path("no_cache", *path),
-            vars_=self.vars_(),
-        )
-
-    def vars_(self):
-        return {"#HOST#": self._router.host}
-
-    @driver_feature(types.Feature.IMPERSONATION)
-    def test_should_resolve_db_per_session_session_run(self):
-        def _test():
-            self.start_server(self._router, "router_change_homedb.script")
-            self.start_server(self._reader1, "reader_change_homedb.script")
-
-            driver = Driver(self._backend, self._uri, self._auth_token)
-
-            session1 = driver.session("r", impersonated_user="the-imposter")
-            result = session1.run("RETURN 1")
-            result.consume()
-            if not parallel_sessions:
-                session1.close()
-
-            session2 = driver.session(
-                "r", bookmarks=["bookmark"], impersonated_user="the-imposter"
-            )
-            result = session2.run("RETURN 2")
-            result.consume()
-            session2.close()
-            if parallel_sessions:
-                session1.close()
-
-            driver.close()
-
-            self._router.done()
-            self._reader1.done()
-
-        for parallel_sessions in (True, False):
-            with self.subTest(parallel_sessions=parallel_sessions):
-                _test()
-            self._router.reset()
+        def tearDown(self):
             self._reader1.reset()
-
-    @driver_feature(types.Feature.IMPERSONATION)
-    def test_should_resolve_db_per_session_tx_run(self):
-        def _test():
-            self.start_server(self._router, "router_change_homedb.script")
-            self.start_server(self._reader1, "reader_tx_change_homedb.script")
-
-            driver = Driver(self._backend, self._uri, self._auth_token)
-
-            session1 = driver.session("r", impersonated_user="the-imposter")
-            tx = session1.begin_transaction()
-            result = tx.run("RETURN 1")
-            result.consume()
-            tx.commit()
-            if not parallel_sessions:
-                session1.close()
-
-            session2 = driver.session(
-                "r", bookmarks=["bookmark"], impersonated_user="the-imposter"
-            )
-            tx = session2.begin_transaction()
-            result = tx.run("RETURN 2")
-            result.consume()
-            tx.commit()
-            session2.close()
-            if parallel_sessions:
-                session1.close()
-
-            driver.close()
-
-            self._router.done()
-            self._reader1.done()
-
-        for parallel_sessions in (True, False):
-            with self.subTest(parallel_sessions=parallel_sessions):
-                _test()
+            self._reader2.reset()
             self._router.reset()
-            self._reader1.reset()
+            super().tearDown()
 
-    @driver_feature(types.Feature.IMPERSONATION)
-    def test_should_resolve_db_per_session_tx_func_run(self):
-        def _test():
-            def work(tx):
-                result = tx.run(query)
+        def start_server(self, server, *path):
+            server.start(
+                path=self.script_path("no_cache", *path),
+                vars_=self.vars_(),
+            )
+
+        def vars_(self):
+            return {"#HOST#": self._router.host}
+
+        @driver_feature(types.Feature.IMPERSONATION)
+        def test_should_resolve_db_per_session_session_run(self):
+            def _test():
+                self.start_server(self._router, "router_change_homedb.script")
+                self.start_server(self._reader1, "reader_change_homedb.script")
+
+                driver = Driver(self._backend, self._uri, self._auth_token)
+
+                session1 = driver.session(
+                    "r", impersonated_user="the-imposter"
+                )
+                result = session1.run("RETURN 1")
                 result.consume()
+                if not parallel_sessions:
+                    session1.close()
 
-            self.start_server(self._router, "router_change_homedb.script")
-            self.start_server(self._reader1, "reader_tx_change_homedb.script")
+                session2 = driver.session(
+                    "r", bookmarks=["bookmark"],
+                    impersonated_user="the-imposter"
+                )
+                result = session2.run("RETURN 2")
+                result.consume()
+                session2.close()
+                if parallel_sessions:
+                    session1.close()
 
-            driver = Driver(self._backend, self._uri, self._auth_token)
+                driver.close()
 
-            session1 = driver.session("r", impersonated_user="the-imposter")
-            query = "RETURN 1"
-            session1.execute_read(work)
-            if not parallel_sessions:
-                session1.close()
-
-            session2 = driver.session(
-                "r", bookmarks=["bookmark"], impersonated_user="the-imposter"
-            )
-            query = "RETURN 2"
-            session2.execute_read(work)
-            session2.close()
-            if parallel_sessions:
-                session1.close()
-
-            driver.close()
-
-            self._router.done()
-            self._reader1.done()
-
-        for parallel_sessions in (True, False):
-            with self.subTest(parallel_sessions=parallel_sessions):
-                _test()
-            self._router.reset()
-            self._reader1.reset()
-
-    @driver_feature(types.Feature.IMPERSONATION)
-    def test_session_should_cache_home_db_despite_new_rt(self):
-        i = 0
-
-        def work(tx):
-            nonlocal i
-            i += 1
-            if i == 1:
-                with self.assertRaises(types.DriverError) as exc:
-                    res = tx.run("RETURN 1")
-                    return res.next()
                 self._router.done()
                 self._reader1.done()
+
+            for parallel_sessions in (True, False):
+                with self.subTest(parallel_sessions=parallel_sessions):
+                    _test()
+                self._router.reset()
+                self._reader1.reset()
+
+        @driver_feature(types.Feature.IMPERSONATION)
+        def test_should_resolve_db_per_session_tx_run(self):
+            def _test():
+                self.start_server(self._router, "router_change_homedb.script")
                 self.start_server(
-                    self._router, "router_explicit_homedb.script"
+                    self._reader1, "reader_tx_change_homedb.script"
                 )
-                self.start_server(self._reader2, "reader_tx_homedb.script")
-                raise exc.exception
-            else:
-                res = tx.run("RETURN 1")
-                return res.next()
 
-        driver = Driver(self._backend, self._uri, self._auth_token)
+                driver = Driver(self._backend, self._uri, self._auth_token)
 
-        self.start_server(self._router, "router_homedb.script")
-        self.start_server(self._reader1, "reader_tx_exits.script")
+                session1 = driver.session(
+                    "r", impersonated_user="the-imposter"
+                )
+                tx = session1.begin_transaction()
+                result = tx.run("RETURN 1")
+                result.consume()
+                tx.commit()
+                if not parallel_sessions:
+                    session1.close()
 
-        session = driver.session("r", impersonated_user="the-imposter")
-        session.execute_read(work)
-        session.close()
+                session2 = driver.session(
+                    "r", bookmarks=["bookmark"],
+                    impersonated_user="the-imposter"
+                )
+                tx = session2.begin_transaction()
+                result = tx.run("RETURN 2")
+                result.consume()
+                tx.commit()
+                session2.close()
+                if parallel_sessions:
+                    session1.close()
 
-        driver.close()
+                driver.close()
 
-        self._router.done()
-        self._reader2.done()
-        self.assertEqual(i, 2)
+                self._router.done()
+                self._reader1.done()
+
+            for parallel_sessions in (True, False):
+                with self.subTest(parallel_sessions=parallel_sessions):
+                    _test()
+                self._router.reset()
+                self._reader1.reset()
+
+        @driver_feature(types.Feature.IMPERSONATION)
+        def test_should_resolve_db_per_session_tx_func_run(self):
+            def _test():
+                def work(tx):
+                    result = tx.run(query)
+                    result.consume()
+
+                self.start_server(self._router, "router_change_homedb.script")
+                self.start_server(
+                    self._reader1, "reader_tx_change_homedb.script"
+                )
+
+                driver = Driver(self._backend, self._uri, self._auth_token)
+
+                session1 = driver.session(
+                    "r", impersonated_user="the-imposter"
+                )
+                query = "RETURN 1"
+                session1.execute_read(work)
+                if not parallel_sessions:
+                    session1.close()
+
+                session2 = driver.session(
+                    "r", bookmarks=["bookmark"],
+                    impersonated_user="the-imposter"
+                )
+                query = "RETURN 2"
+                session2.execute_read(work)
+                session2.close()
+                if parallel_sessions:
+                    session1.close()
+
+                driver.close()
+
+                self._router.done()
+                self._reader1.done()
+
+            for parallel_sessions in (True, False):
+                with self.subTest(parallel_sessions=parallel_sessions):
+                    _test()
+                self._router.reset()
+                self._reader1.reset()
+
+        @driver_feature(types.Feature.IMPERSONATION)
+        def test_session_should_cache_home_db_despite_new_rt(self):
+            i = 0
+
+            def work(tx):
+                nonlocal i
+                i += 1
+                if i == 1:
+                    with self.assertRaises(types.DriverError) as exc:
+                        res = tx.run("RETURN 1")
+                        return res.next()
+                    self._router.done()
+                    self._reader1.done()
+                    self.start_server(
+                        self._router, "router_explicit_homedb.script"
+                    )
+                    self.start_server(self._reader2, "reader_tx_homedb.script")
+                    raise exc.exception
+                else:
+                    res = tx.run("RETURN 1")
+                    return res.next()
+
+            driver = Driver(self._backend, self._uri, self._auth_token)
+
+            self.start_server(self._router, "router_homedb.script")
+            self.start_server(self._reader1, "reader_tx_exits.script")
+
+            session = driver.session("r", impersonated_user="the-imposter")
+            session.execute_read(work)
+            session.close()
+
+            driver.close()
+
+            self._router.done()
+            self._reader2.done()
+            self.assertEqual(i, 2)
 
 
-class Test4x4HomeDbWithoutCache(_HomeDbWithoutCacheTestCase):
+class Test4x4HomeDbWithoutCache(_Base.HomeDbWithoutCache):
 
     required_features = types.Feature.BOLT_4_4,
 
@@ -207,7 +223,7 @@ class Test4x4HomeDbWithoutCache(_HomeDbWithoutCacheTestCase):
         super().test_session_should_cache_home_db_despite_new_rt()
 
 
-class Test5x8HomeDbWithoutCache(_HomeDbWithoutCacheTestCase):
+class Test5x8HomeDbWithoutCache(_Base.HomeDbWithoutCache):
 
     required_features = types.Feature.BOLT_5_8,
 
@@ -645,7 +661,7 @@ class TestHomeDbWithCache(TestkitTestCase):
             session.close()
             driver.close()
 
-        for resolved_from_cache in (True, False)[1:]:
+        for resolved_from_cache in (True, False):
             with self.subTest(resolved_from_cache=resolved_from_cache):
                 try:
                     _test(resolved_from_cache)
