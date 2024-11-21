7d7eed98b12af607527b8199609ae04fa2c1f109
Robsdedude <rouven.bauer@neo4j.com>
2024-11-21 12:32:32 +0000

    Update sever-bolt support matrix

--- tests/neo4j/shared.py
+++ tests/neo4j/shared.py
@@ -135,10 +135,11 @@ class ServerInfo:
             version = (int(match.group(1)), float("inf"))
         else:
             version = tuple(int(i) for i in self.version.split(".")[:2])
-        if version >= (5, 25):
+        if version >= (5, 26):
             return "5.7"
         if version >= (5, 23):
             return "5.6"
+        # bolt 5.5 was never released
         if version >= (5, 13):
             return "5.4"
         if version >= (5, 9):
