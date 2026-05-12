cbb097b24c0ffa8d90d51e9b96e327e4d3182523
Richard Irons <richard.irons@neo4j.com>
2026-05-12 17:18:32 +0100

    Reorder tests

--- tests/stub/datatypes/test_uuid.py
+++ tests/stub/datatypes/test_uuid.py
@@ -40,6 +40,37 @@ class TestUuid(TestkitTestCase):
         auth = types.AuthorizationToken("basic", principal="", credentials="")
         return Driver(self._backend, uri, auth)
 
+    def test_uuid(self):
+        script = "echo_uuid.script"
+        for value in (
+            uuid.UUID("00000000000000000000000000000000"),
+            uuid.UUID("ffffffffffffffffffffffffffffffff"),
+            uuid.UUID("0102030405060708090a0b0c0d0e0f10"),
+            uuid.uuid4(),
+        ):
+            with self.subTest(value=str(value)):
+                with self._started_server(
+                    self._server,
+                    script,
+                    vars_={
+                        "#UUID#": json.dumps({"UU": str(value)}),
+                    },
+                ):
+                    with self._driver(self._server) as driver:
+                        with driver.session("r") as session:
+                            cypher_uuid = types.CypherUUID(value)
+                            result = session.run(
+                                "RETURN $uuid AS uuid",
+                                params={"uuid": cypher_uuid},
+                            )
+                            records = list(result)
+                            self._server.done()
+                            self.assertEqual(len(records), 1)
+                            fields = records[0].values
+                            self.assertEqual(len(fields), 1)
+                            self.assertEqual(cypher_uuid, fields[0])
+
+
     def test_uuid_in_list(self):
         script = "echo_uuid_list.script"
         uuid_pairs = [
@@ -71,33 +102,3 @@ class TestUuid(TestkitTestCase):
                             fields = records[0].values
                             self.assertEqual(len(fields), 1)
                             self.assertEqual(cypher_uuids, fields[0])
-
-    def test_uuid(self):
-        script = "echo_uuid.script"
-        for value in (
-            uuid.UUID("00000000-0000-0000-0000-000000000000"),
-            uuid.UUID("ffffffff-ffff-ffff-ffff-ffffffffffff"),
-            uuid.UUID("01020304-0506-0708-090a-0b0c0d0e0f10"),
-            uuid.uuid4(),
-        ):
-            with self.subTest(value=str(value)):
-                with self._started_server(
-                    self._server,
-                    script,
-                    vars_={
-                        "#UUID#": json.dumps({"UU": str(value)}),
-                    },
-                ):
-                    with self._driver(self._server) as driver:
-                        with driver.session("r") as session:
-                            cypher_uuid = types.CypherUUID(value)
-                            result = session.run(
-                                "RETURN $uuid AS uuid",
-                                params={"uuid": cypher_uuid},
-                            )
-                            records = list(result)
-                            self._server.done()
-                            self.assertEqual(len(records), 1)
-                            fields = records[0].values
-                            self.assertEqual(len(fields), 1)
-                            self.assertEqual(cypher_uuid, fields[0])
