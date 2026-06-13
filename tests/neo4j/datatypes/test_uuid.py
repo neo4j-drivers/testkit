06e494acdb3ab5b159e79a67045cac66eb03bed3
Richard Irons <richard.irons@neo4j.com>
2026-06-13 18:58:50 +0100

    Fix flake8 line length violations

--- tests/neo4j/datatypes/test_uuid.py
+++ tests/neo4j/datatypes/test_uuid.py
@@ -15,7 +15,7 @@ class TestUuidTypes(_TestTypesBase):
         values = [
             uuid.UUID("00000000-0000-0000-0000-000000000000"),  # nil UUID
             uuid.UUID("ffffffff-ffff-ffff-ffff-ffffffffffff"),  # all-ones
-            uuid.UUID("01020304-0506-0708-090a-0b0c0d0e0f12"),  # sequential bytes
+            uuid.UUID("01020304-0506-0708-090a-0b0c0d0e0f12"),  # sequential
             uuid.uuid4(),
         ]
         self._create_driver_and_session()
@@ -25,9 +25,11 @@ class TestUuidTypes(_TestTypesBase):
 
     def test_uuid_in_list(self):
         self._create_driver_and_session()
+        nil = uuid.UUID("00000000-0000-0000-0000-000000000000")
+        ones = uuid.UUID("ffffffff-ffff-ffff-ffff-ffffffffffff")
         data = types.CypherList([
-            types.CypherUUID(uuid.UUID("00000000-0000-0000-0000-000000000000")),
-            types.CypherUUID(uuid.UUID("ffffffff-ffff-ffff-ffff-ffffffffffff")),
+            types.CypherUUID(nil),
+            types.CypherUUID(ones),
             types.CypherUUID(uuid.uuid4()),
         ])
         self._verify_can_echo(data)
